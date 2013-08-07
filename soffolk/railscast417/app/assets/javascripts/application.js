c37997925f10ea02f5ab45fc2a2d15488255fb85
soffolk <zlx.star@gmail.com>
2013-08-07 22:01:51 +0800

    add railscast417

---
+++ soffolk/railscast417/app/assets/javascripts/application.js
@@ -0,0 +1,33 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require foundation/foundation
+//= require foundation/foundation.alerts
+//= require foundation/foundation.clearing
+//= require foundation/foundation.cookie
+//= require foundation/foundation.dropdown
+//= require foundation/foundation.forms
+//= require foundation/foundation.magellan
+//= require foundation/foundation.orbit
+//= require foundation/foundation.reveal
+//= require foundation/foundation.section
+//= require foundation/foundation.tooltips
+//= require foundation/foundation.topbar
+//= require foundation/foundation.interchange
+//= require foundation/foundation.placeholder
+//= require foundation/foundation.abide
+//= require turbolinks
+//= require_tree .
+
+$(function(){ $(document).foundation(); });
---
+++ soffolk/railscast417/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Railscast417::Application.config.session_store :cookie_store, key: '_railscast417_session'
