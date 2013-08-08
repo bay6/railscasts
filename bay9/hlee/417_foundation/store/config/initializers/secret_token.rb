6c0ffdec0aa9c816eb0183562d4e23885e1b9f6b
Ken Lu <ken@seeice.com>
2013-08-07 20:24:09 -0400

    initial and scaffold

---
+++ bay9/hlee/417_foundation/store/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Store::Application.config.secret_key_base = '8709cdc7c03312130d3a1c48088b47fc0d649fdc9a0dc61ed141e729cac572e6f870c75a9141eee0f0905277f5a010c2925c1df1dd76398dd71b91ace71f5217'
