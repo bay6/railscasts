decb10e65bba97034be9da15a605a3c5d0335d8d
caok <caok1231@gmail.com>
2013-08-08 18:48:03 +0800

    init and add foundation

---
+++ caok/417/foundation/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Foundation::Application.config.secret_key_base = '010162d84b6e86a51f65855fa4c9b9ca09bdc7ff21062086396653c81249c02ceff3e830e58f1ccba551f2e0a586d4049857fe753f17fe7654c77a3b1af8efcb'
