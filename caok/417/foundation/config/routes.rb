e596dee540b7e042409ba89d54b5e6cedd398806
caok <caok1231@gmail.com>
2013-08-08 19:03:24 +0800

    add readme

--- caok/417/foundation/config/routes.rb
+++ caok/417/foundation/config/routes.rb
@@ -1,5 +1,6 @@
 Foundation::Application.routes.draw do
   resources :products
+  root 'products#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
