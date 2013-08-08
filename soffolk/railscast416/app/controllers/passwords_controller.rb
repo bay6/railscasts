03d6ee5a6efebc277b5b3cddf1533cd9968d2af0
soffolk <zlx.star@gmail.com>
2013-08-08 23:42:26 +0800

    add railscast416

---
+++ soffolk/railscast416/app/controllers/passwords_controller.rb
@@ -0,0 +1,14 @@
+class PasswordsController < ApplicationController
+  def new
+    @password_form = PasswordForm.new(current_user)
+  end
+
+  def create
+    @password_form = PasswordForm.new(current_user)
+    if @password_form.submit(params[:password_form])
+      redirect_to current_user, notice: "Successfully changed password."
+    else
+      render "new"
+    end
+  end
+end
