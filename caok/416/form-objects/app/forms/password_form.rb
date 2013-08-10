62adeb0a4240c6a0cb1168ad2274663f8cc2f641
caok <caok1231@gmail.com>
2013-08-10 13:03:15 +0800

    add password_form

---
+++ caok/416/form-objects/app/forms/password_form.rb
@@ -0,0 +1,40 @@
+class PasswordForm
+  # Rails 4: include ActiveModel::Model
+  extend ActiveModel::Naming
+  include ActiveModel::Conversion
+  include ActiveModel::Validations
+
+  def persisted?
+    false
+  end
+
+  attr_accessor :original_password, :new_password
+
+  validate :verify_original_password
+  validates_presence_of :original_password, :new_password
+  validates_confirmation_of :new_password
+  validates_length_of :new_password, minimum: 6
+
+  def initialize(user)
+    @user = user
+  end
+
+  def submit(params)
+    self.original_password = params[:original_password]
+    self.new_password = params[:new_password]
+    self.new_password_confirmation = params[:new_password_confirmation]
+    if valid?
+      @user.password = new_password
+      @user.save!
+      true
+    else
+      false
+    end
+  end
+
+  def verify_original_password
+    unless @user.authenticate(original_password)
+      errors.add :original_password, "is not correct"
+    end
+  end
+end
