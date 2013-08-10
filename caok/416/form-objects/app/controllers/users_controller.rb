09b6c3135a953ff2d8a26d5287c54e0f836423af
caok <caok1231@gmail.com>
2013-08-10 13:43:24 +0800

    add signup_form

--- caok/416/form-objects/app/controllers/users_controller.rb
+++ caok/416/form-objects/app/controllers/users_controller.rb
@@ -1,14 +1,13 @@
 class UsersController < ApplicationController
   def new
-    @user = User.new
-    @user.build_profile
+    @signup_form = SignupForm.new
   end
 
   def create
-    @user = User.new(user_params)
-    if @user.save
-      session[:user_id] = @user.id
-      redirect_to @user, notice: "Thank you for signing up!"
+    @signup_form = SignupForm.new
+    if @signup_form.submit(params[:user])
+      session[:user_id] = @signup_form.user.id
+      redirect_to @signup_form, notice: "Thank you for signing up!"
     else
       render "new"
     end
@@ -16,14 +15,5 @@ class UsersController < ApplicationController
 
   def show
     @user = current_user
-  end
-
-private
-
-  def user_params
-    params.require(:user).permit(
-      :username, :email, :password, :password_confirmation, :subscribed,
-      profile_attributes: [:twitter_name, :github_name, :bio]
-    )
   end
 end
