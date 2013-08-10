5124408e567e33b02fb848635bc221a5bfbced89
William Herry <WilliamHerryChina@Gmail.com>
2013-08-10 21:20:42 +0800

    railscast 416 by william

---
+++ williamherry/railscasts416/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Example::Application.config.secret_token = '31858d1e971647aae19d83652cf23aa619a76f3ee5e9167dea700eec4d7bf1a578cd91c8be0c2f5eb37f7a2cffad9db9655d331d51f0748fde5681437e731d73'
