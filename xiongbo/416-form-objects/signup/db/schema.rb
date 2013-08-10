83d599a5454b050568b591b6f90087dded212e52
xiongbo <xiongbo027@gmail.com>
2013-08-10 10:02:33 +0800

    finished password change

--- xiongbo/416-form-objects/signup/db/schema.rb
+++ xiongbo/416-form-objects/signup/db/schema.rb
@@ -9,26 +9,26 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130602234500) do
+ActiveRecord::Schema.define(version: 20130602234500) do
 
-  create_table "profiles", :force => true do |t|
+  create_table "profiles", force: true do |t|
     t.integer  "user_id"
     t.string   "twitter_name"
     t.string   "github_name"
     t.text     "bio"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  add_index "profiles", ["user_id"], :name => "index_profiles_on_user_id"
+  add_index "profiles", ["user_id"], name: "index_profiles_on_user_id"
 
-  create_table "users", :force => true do |t|
+  create_table "users", force: true do |t|
     t.string   "email"
     t.string   "password_digest"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "token"
     t.string   "username"
     t.datetime "subscribed_at"
