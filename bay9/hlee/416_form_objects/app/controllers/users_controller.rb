6fa83f0d9ce30a39fb0cbb7d5843d0c88cbe2895
Ken Lu <ken@seeice.com>
2013-08-10 06:09:05 -0400

    Finished form object #4

--- bay9/hlee/416_form_objects/app/controllers/users_controller.rb
+++ bay9/hlee/416_form_objects/app/controllers/users_controller.rb
@@ -4,10 +4,10 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
-    if @user.save
-      session[:user_id] = @user.id
-      redirect_to @user, notice: "Thank you for signing up!"
+    @signup_form = SignupForm.new
+    if @signup_form.submit(params[:user]) 
+      session[:user_id] = @signup_form.user.id
+      redirect_to @signup_form.user, notice: "Thank you for signing up!"
     else
       render "new"
     end
@@ -17,12 +17,4 @@ class UsersController < ApplicationController
     @user = current_user
   end
 
-private
-
-  def user_params
-    params.require(:user).permit(
-      :username, :email, :password, :password_confirmation, :subscribed,
-      profile_attributes: [:twitter_name, :github_name, :bio]
-    )
-  end
 end
