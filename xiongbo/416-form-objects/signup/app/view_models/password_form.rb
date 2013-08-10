7dde06692758fdbcce2bcf99a349a380fb80617e
xiongbo <xiongbo027@gmail.com>
2013-08-10 10:02:33 +0800

    finished password change

--- xiongbo/416-form-objects/signup/app/view_models/password_form.rb
+++ xiongbo/416-form-objects/signup/app/view_models/password_form.rb
@@ -3,9 +3,9 @@ class PasswordForm
 
   attr_accessor :original_password, :new_password
   validate :verify_original_password
-  validate_presence_of :original_password, :new_password
-  validate_confirmation_of :new_password
-  validate_length_of :new_password, minimum: 6
+  validates_presence_of :original_password, :new_password
+  validates_confirmation_of :new_password
+  validates_length_of :new_password, minimum: 6
 
   def initialize(user)
     @user = user
@@ -19,5 +19,20 @@ class PasswordForm
 
   def change_password
     @user.password = new_password
+  end
+
+  def submit(params)
+    self.original_password = params[:original_password]
+    self.new_password = params[:new_password]
+    self.new_password_confirmation = params[:new_password_confirmation]
+
+    if valid?
+      @user.password = new_password
+      @user.password_confirmation = new_password
+      @user.save!
+      true 
+    else
+      false
+    end
   end
 end
