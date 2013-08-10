bda010a60c79446bfd141d6852694cb9848bea66
Ken Lu <ken@seeice.com>
2013-08-10 05:04:57 -0400

    finished password form_object

--- bay9/hlee/416_form_objects/app/forms/password_form.rb
+++ bay9/hlee/416_form_objects/app/forms/password_form.rb
@@ -1,8 +1,8 @@
 class PasswordForm
   # Rails 4: include ActiveRecord::Model
-  extend ActiveRecord::Naming
-  include ActiveRecord::Conversion
-  include ActiveRecord::Validations
+  extend ActiveModel::Naming
+  include ActiveModel::Conversion
+  include ActiveModel::Validations
 
   def persisted?
     false
@@ -14,4 +14,28 @@ class PasswordForm
   validates_presence_of :original_password, :new_password
   validates_confirmation_of :new_password
   validates_length_of :new_password, minimum: 6
+
+  def initialize(user)
+    @user = user
+  end
+
+  def submit(params)
+     self.original_password = params[:original_password]
+     self.new_password = params[:new_password]
+     self.new_password_confirmation = params[:new_password_confirmation]
+     if valid?
+       @user.password = new_password 
+       @user.save!
+       true
+     else 
+       false
+     end
+  end
+
+  def verify_original_password
+    unless @user.authenticate(original_password)
+      errors.add :original_password, "is not correct"
+    end
+  end
+
 end
