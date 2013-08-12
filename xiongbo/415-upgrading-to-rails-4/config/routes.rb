79128900dce11e1c86c86ace9bf444fc9e79053c
xiongbo <xiongbo027@gmail.com>
2013-08-12 21:44:57 +0800

    update routes

--- xiongbo/415-upgrading-to-rails-4/config/routes.rb
+++ xiongbo/415-upgrading-to-rails-4/config/routes.rb
@@ -1,7 +1,7 @@
 Screencaster::Application.routes.draw do
   resources :episodes do
     collection do
-      put :update_many
+      patch :update_many
     end
   end
   get 'new', to: 'episodes#new'
