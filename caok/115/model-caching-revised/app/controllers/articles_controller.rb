a9fee98d62aa95b8e8cc37edcf98d69ef1af6749
caok <caok1231@gmail.com>
2013-08-12 21:06:49 +0800

    add cached_published

--- caok/115/model-caching-revised/app/controllers/articles_controller.rb
+++ caok/115/model-caching-revised/app/controllers/articles_controller.rb
@@ -4,7 +4,7 @@ class ArticlesController < ApplicationController
   # GET /articles
   # GET /articles.json
   def index
-    @articles = Article.all
+    @articles = Article.cached_published
   end
 
   # GET /articles/1
