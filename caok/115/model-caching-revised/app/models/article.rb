2635a5bec9cab3fd46802907ef25722907eff4a0
caok <caok1231@gmail.com>
2013-08-12 20:43:24 +0800

    add cached_comments_count

--- caok/115/model-caching-revised/app/models/article.rb
+++ caok/115/model-caching-revised/app/models/article.rb
@@ -1,3 +1,7 @@
 class Article < ActiveRecord::Base
   has_many :comments
+
+  def cached_comments_count
+    Rails.cache.fetch([self, 'comments_count']) { comments.size }
+  end
 end
--- caok/115/model-caching-revised/app/models/comment.rb
+++ caok/115/model-caching-revised/app/models/comment.rb
@@ -1,3 +1,3 @@
 class Comment < ActiveRecord::Base
-  belongs_to :article
+  belongs_to :article, touch: true
 end
