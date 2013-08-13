509b8f0da9b5bf1e4340b3af2793ee1c278b15bc
Ken Lu <ken@seeice.com>
2013-08-12 21:53:07 -0400

    cached counter updated #23

--- bay9/hlee/115r_model_caching/app/models/article.rb
+++ bay9/hlee/115r_model_caching/app/models/article.rb
@@ -3,4 +3,13 @@ class Article < ActiveRecord::Base
   has_many :comments
   attr_accessible :name, :published_at, :content
   scope :published, -> { where("published_at < ?", Time.zone.now) }
+
+  def cached_comments_count
+    Rails.cache.fetch([self, 'comments_count']) { comments.size }
+  end
+
+  def cached_comments
+    Rails.cache.fetch([self, 'comments']) { comments.to_s }
+  end
+
 end
--- bay9/hlee/115r_model_caching/app/models/comment.rb
+++ bay9/hlee/115r_model_caching/app/models/comment.rb
@@ -1,4 +1,4 @@
 class Comment < ActiveRecord::Base
-  belongs_to :article
+  belongs_to :article, touch: true
   attr_accessible :name, :content
 end
