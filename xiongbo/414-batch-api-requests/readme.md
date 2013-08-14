bb2f2f7b26a10527e236c80957b2c7e68b02db3c
xiongbo <xiongbo027@gmail.com>
2013-08-14 18:02:58 +0800

    update task.js

--- xiongbo/414-batch-api-requests/readme.md
+++ xiongbo/414-batch-api-requests/readme.md
@@ -2,6 +2,7 @@
 
 ## resources
 [rack spec](http://rack.rubyforge.org/doc/SPEC.html)
+[stringify](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/stringify)
 
 ## pay attention to
 1. env.inspect
--- xiongbo/414-batch-api-requests/todo/app/middleware/batch_requests.rb
+++ xiongbo/414-batch-api-requests/todo/app/middleware/batch_requests.rb
@@ -22,7 +22,7 @@ class BatchRequests
     env["QUERY_STRING"] = query
     env["rack.input"] = StringIO.new(request['body'].to_s)
     status, headers, body = @app.call(env) 
-    body.close
+    body.close if body.respond_to? :close
     {status: status, headers: headers, body: body.join}
   end
 end
