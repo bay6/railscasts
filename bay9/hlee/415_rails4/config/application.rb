474c9c0ec689d323b88fc9e4f2ae0048dd601669
Ken Lu <ken@seeice.com>
2013-08-14 08:32:06 -0400

    added more gem change and bundler

--- bay9/hlee/415_rails4/config/application.rb
+++ bay9/hlee/415_rails4/config/application.rb
@@ -2,13 +2,7 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-if defined?(Bundler)
-  # If you precompile assets before deploying to production, use this line
-  Bundler.require(*Rails.groups(:assets => %w(development test)))
-  # If you want your assets lazily compiled in production, use this line
-  # Bundler.require(:default, :assets, Rails.env)
-end
-
+Bundler.require(:default, Rails.env)
 module Screencaster
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
