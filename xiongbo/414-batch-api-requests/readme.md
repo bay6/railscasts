d167895d794cab0bf851d9142ef794fbd32a1ea5
xiongbo <xiongbo027@gmail.com>
2013-08-14 16:12:50 +0800

    finished middleware

--- xiongbo/414-batch-api-requests/readme.md
+++ xiongbo/414-batch-api-requests/readme.md
@@ -2,3 +2,9 @@
 
 ## resources
 [rack spec](http://rack.rubyforge.org/doc/SPEC.html)
+
+## pay attention to
+1. env.inspect
+2. rack.input
+3. StringIO
+4. request = Rack.Request.new(env.deep_dup)
--- xiongbo/414-batch-api-requests/todo/app/middleware/batch_requests.rb
+++ xiongbo/414-batch-api-requests/todo/app/middleware/batch_requests.rb
@@ -5,13 +5,24 @@ class BatchRequests
 
   def call(env) 
     if env["PATH_INFO"] == '/batch'
-      env["REQUEST_METHOD"] = "GET"
-      env["PATH_INFO"] = "/tasks.json"
-      env["QUERY_STRING"] = ""
-      env["rack.input"] = StringIO.new("")
-      @app.call(env)
+      request = Rack::Request.new(env.deep_dup)
+      responses = JSON.parse(request[:requests]).map do |request|
+        process_request(env.deep_dup, request)
+      end
+      [200, {"Content-Type" => "application/json"}, [{response: responses}.to_json]]
     else 
       @app.call(env) 
     end
+  end
+
+  def process_request(env, request) 
+    path, query = request['url'].split("?")
+    env["REQUEST_METHOD"] = request['method']
+    env["PATH_INFO"] = path
+    env["QUERY_STRING"] = query
+    env["rack.input"] = StringIO.new(request['body'].to_s)
+    status, headers, body = @app.call(env) 
+    body.close
+    {status: status, headers: headers, body: body.join}
   end
 end
