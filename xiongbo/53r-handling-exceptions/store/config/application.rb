be02eff113b0ee0c2e7b902ceb3cb65b79ecab1f
xiongbo <xiongbo027@gmail.com>
2013-08-15 17:11:32 +0800

    add custom forbidden exception

--- xiongbo/53r-handling-exceptions/store/config/application.rb
+++ xiongbo/53r-handling-exceptions/store/config/application.rb
@@ -58,5 +58,7 @@ module Store
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.action_dispatch.rescue_responses["ProductsController::Forbidden"] = :forbidden
   end
 end
