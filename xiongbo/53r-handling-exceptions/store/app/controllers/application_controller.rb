b6f3e1c8e85c2c0fce6f92c06a8df412e8d950de
xiongbo <xiongbo027@gmail.com>
2013-08-15 21:21:07 +0800

    finished dynamic exception handling

--- xiongbo/53r-handling-exceptions/store/app/controllers/application_controller.rb
+++ xiongbo/53r-handling-exceptions/store/app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  rescue_from "Exception", with: :forbidden
+
+  private
+
+  def forbidden(exception)
+    render text: exception.message
+  end
+
 end
