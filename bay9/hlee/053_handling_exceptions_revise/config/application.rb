40589b12a73b97e597ae1cad455348576332ae71
Ken Lee <ken@seeice.com>
2013-08-16 16:08:44 -0400

    for forbidden

--- bay9/hlee/053_handling_exceptions_revise/config/application.rb
+++ bay9/hlee/053_handling_exceptions_revise/config/application.rb
@@ -46,7 +46,9 @@ module Store
     # This will create an empty whitelist of attributes available for mass-assignment for all models
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
     # parameters by using an attr_accessible or attr_protected declaration.
-    config.active_record.whitelist_attributes = true
+    config.active_record.whitelist_attributes = false
+
+    config.action_dispatch.rescue_responses["ProductsController::Forbidden"] = :forbidden
 
     # Enable the asset pipeline
     config.assets.enabled = true
