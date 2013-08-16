a642923c215209cb45f8f4b38f607ac0d486959e
Ken Lee <ken@seeice.com>
2013-08-16 15:23:44 -0400

    updated rails 4 of configuration changed

--- bay9/hlee/053_handling_exceptions_revise/config/environments/production.rb
+++ bay9/hlee/053_handling_exceptions_revise/config/environments/production.rb
@@ -20,6 +20,8 @@ Store::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  config.eager_load = false
+
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
