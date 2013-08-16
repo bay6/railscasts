e33570b69c82b7b2c7484c9d457d550aa28e0e4d
Ken Lu <ken@seeice.com>
2013-08-16 09:06:21 -0400

    added Fast Rails Commands which is not much

---
+++ 412_after/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '51da8461993a16a5285d71923ddb7286b0902e617b4efd41d5f808d94bc2e0d917327541d56fe1e1bf385a3b4ad09bc84853d561be16c03853bbec0d0ccd13fe'
