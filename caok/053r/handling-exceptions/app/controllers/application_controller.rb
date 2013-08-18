f041037ff82f8156ec352dd9cd0e4130ad825172
caok <caok1231@gmail.com>
2013-08-18 11:39:07 +0800

    change into Exception

--- caok/053r/handling-exceptions/app/controllers/application_controller.rb
+++ caok/053r/handling-exceptions/app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  rescue_from "ProductsController::Forbidden", with: :forbidden
+  rescue_from "Exception", with: :forbidden
 
+  private
   def forbidden(exception)
     render text: exception.message
   end
