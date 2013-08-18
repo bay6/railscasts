c764704222a4702866e2b967dbea62557c83a190
Ken Lu <ken@seeice.com>
2013-08-18 09:34:36 -0400

    added GC.disable howwever seems slower

--- bay9/hlee/413_fast_test/spec/spec_helper.rb
+++ bay9/hlee/413_fast_test/spec/spec_helper.rb
@@ -31,4 +31,6 @@ RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.filter_run focus: true
   config.run_all_when_everything_filtered = true
+  config.before(:each) { GC.disable }
+  config.after(:each) {GC.enable}
 end
