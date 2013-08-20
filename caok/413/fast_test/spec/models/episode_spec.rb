0f84150519ca499c47cfce2d46594102c6848c88
caok <caok1231@gmail.com>
2013-08-20 10:08:40 +0800

    change create to build

--- caok/413/fast_test/spec/models/episode_spec.rb
+++ caok/413/fast_test/spec/models/episode_spec.rb
@@ -16,28 +16,28 @@ describe Episode do
   end
 
   it "translates single digit seconds into timecode with minutes" do
-    create(:episode, seconds: 60*8+3).timecode.should eq('8:03')
+    build(:episode, seconds: 60*8+3).timecode.should eq('8:03')
   end
 
   it "translates double digit seconds into timecode with minutes" do
-    create(:episode, seconds: 60*8+12).timecode.should eq('8:12')
+    build(:episode, seconds: 60*8+12).timecode.should eq('8:12')
   end
 
   it "returns nil for timecode if seconds aren't set" do
-    create(:episode, seconds: nil).timecode.should be_nil
+    build(:episode, seconds: nil).timecode.should be_nil
   end
 
   it "parses timecode into seconds" do
-    create(:episode, timecode: '10:03').seconds.should eq(603)
-    create(:episode, timecode: '').seconds.should be_nil
+    build(:episode, timecode: '10:03').seconds.should eq(603)
+    build(:episode, timecode: '').seconds.should be_nil
   end
 
   it "returns nil for minutes if seconds aren't set" do
-    create(:episode, seconds: nil).minutes.should be_nil
+    build(:episode, seconds: nil).minutes.should be_nil
   end
 
   it "rounds the number of minutes" do
-    create(:episode, seconds: 60).minutes.should eq(1)
-    create(:episode, seconds: 91).minutes.should eq(2)
+    build(:episode, seconds: 60).minutes.should eq(1)
+    build(:episode, seconds: 91).minutes.should eq(2)
   end
 end
