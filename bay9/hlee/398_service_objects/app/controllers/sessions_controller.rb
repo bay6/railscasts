57073c7eb840ef8a63a125882ae9b5f368027de2
Ken Lu <ken@seeice.com>
2013-08-21 10:46:28 -0400

    moved authenticate to app service

--- bay9/hlee/398_service_objects/app/controllers/sessions_controller.rb
+++ bay9/hlee/398_service_objects/app/controllers/sessions_controller.rb
@@ -3,17 +3,27 @@ class SessionsController < ApplicationController
   end
 
   def create
-    if auth = env['omniauth.auth']
-    @user = User.where(auth.slice(:provider, :uid)).first_or_initialize.tap do |usr|
-      usr.provider = auth[:provider]
-      usr.uid = auth[:uid]
-      usr.username = auth[:info][:nickname]
-      usr.save!
-    end
+    auth = Authentication.new(params, env['omniauth.auth'])
+    if auth.authenticated?
+      session[:user_id] = auth.user.id
+      redirect_to root_url, notice: 'Logged in!'
     else
-      user = User.find_by_username(params[:username])
-      @user = user if user and user.authenticate(params[:username], params[:password])
+      flash.now.alert = 'Username or passord is invalid'
+      render 'new'
     end
+    #if auth = env['omniauth.auth']
+    #@user = User.where(auth.slice(:provider, :uid)).first_or_initialize.tap do |usr|
+      #usr.provider = auth[:provider]
+      #usr.uid = auth[:uid]
+      #usr.username = auth[:info][:nickname]
+      #usr.save!
+    #end
+    #else
+      #user = User.find_by_username(params[:username])
+      #@user = user if user and user.authenticate(params[:username], params[:password])
+    #end
+
+
     #if env["omniauth.auth"]
       #@user = User.from_omniauth(env["omniauth.auth"])
     #else
