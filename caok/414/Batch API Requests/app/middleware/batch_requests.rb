6761152fdc40b1f047e8face7f28fc47ae306cfa
caok <caok1231@gmail.com>
2013-08-22 22:33:04 +0800

    add Rack::Request

--- caok/414/Batch API Requests/app/middleware/batch_requests.rb
+++ caok/414/Batch API Requests/app/middleware/batch_requests.rb
@@ -5,16 +5,24 @@ class BatchRequests
 
   def call(env)
     if env["PATH_INFO"] == "/batch"
-      env["REQUEST_METHOD"] = "GET"
-      env["PATH_INFO"] = "/tasks.json"
-      env["QUERY_STRING"] = ""
-      env["rack.input"] = StringIO.new("")
-      status, headers, body = @app.call(env)
-      body.close if body.respond_to? :close
-      response = {status: status, headers: headers, body: body}
-      [200, {"Content-Type" => "application/json"}, [{responses: [response]}.to_json]]
+      request = Rack::Request.new(env.deep_dup)
+      responses = JSON.parse(request[:requests]).map do |override|
+        process_request(env.deep_dup, override)
+      end
+      [200, {"Content-Type" => "application/json"}, [{responses: responses}.to_json]]
     else
       @app.call(env)
     end
+  end
+
+  def process_request(env, override)
+    path, query = override["url"].split("?")
+    env["REQUEST_METHOD"] = override["method"]
+    env["PATH_INFO"] = path
+    env["QUERY_STRING"] = query
+    env["rack.input"] = StringIO.new(override["body"].to_s)
+    status, headers, body = @app.call(env)
+    body.close if body.respond_to? :close
+    {status: status, headers: headers, body: body.join}
   end
 end
