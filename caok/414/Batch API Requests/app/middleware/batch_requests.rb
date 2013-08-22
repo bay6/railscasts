f3cf8701e389894817d6ee497eadf4004b14b492
caok <caok1231@gmail.com>
2013-08-22 22:12:51 +0800

    modify the env in batch_requests

--- caok/414/Batch API Requests/app/middleware/batch_requests.rb
+++ caok/414/Batch API Requests/app/middleware/batch_requests.rb
@@ -3,11 +3,16 @@ class BatchRequests
     @app = app
   end
 
-  #def call(env)
-    #if env["PATH_INFO"] == "/batch"
+  def call(env)
+    if env["PATH_INFO"] == "/batch"
+      env["REQUEST_METHOD"] = "GET"
+      env["PATH_INFO"] = "/tasks.json"
+      env["QUERY_STRING"] = ""
+      env["rack.input"] = StringIO.new("")
+      @app.call(env)
       #[200, {"Content-Type" => "application/json"}, [env.inspect]]
-    #else
-      #@app.call(env)
-    #end
-  #end
+    else
+      @app.call(env)
+    end
+  end
 end
