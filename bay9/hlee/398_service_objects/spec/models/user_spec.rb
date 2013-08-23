88e0d8f7af094f6fe367fbf691e1961f3ee3af5e
Ken Lee <ken@seeice.com>
2013-08-23 14:06:45 -0400

    finished invitation Fixed #76

--- bay9/hlee/398_service_objects/spec/models/user_spec.rb
+++ bay9/hlee/398_service_objects/spec/models/user_spec.rb
@@ -11,22 +11,4 @@ describe User do
     end
   end
 
-  describe '#send_invitation' do
-    it "delivers email and increments count" do
-      user = create(:user)
-      user.send_invitation("foo@example.com")
-      user.invitation_count.should eq(1)
-      last_email.to.should include("foo@example.com")
-    end
-  end
-
-  describe '#reached_invitation_limit?' do
-    it "if invitation count is more than 5" do
-      user = build(:user)
-      user.invitation_count = 5
-      user.reached_invitation_limit?.should be_false
-      user.invitation_count = 6
-      user.reached_invitation_limit?.should be_true
-    end
-  end
 end
