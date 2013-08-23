f9eed38a85b4a8ce88443246463d0eb431c09459
Ken Lu <ken@seeice.com>
2013-08-23 08:45:25 -0400

    added new services of password reset

--- bay9/hlee/398_service_objects/app/controllers/password_resets_controller.rb
+++ bay9/hlee/398_service_objects/app/controllers/password_resets_controller.rb
@@ -3,9 +3,12 @@ class PasswordResetsController < ApplicationController
   end
 
   def create
-    user = User.find_by_email(params[:email])
-    if user
-      user.send_password_reset
+    password_reset = PasswordReset.from_email(params[:email])
+    #user = User.find_by_email(params[:email])
+    #if user
+    if password_reset.user
+      #user.send_password_reset
+      password_reset.send_email
       redirect_to root_url, notice: "Email sent with password reset instructions."
     else
       redirect_to new_password_reset_url, alert: "Email address does not match a user account."
@@ -13,12 +16,17 @@ class PasswordResetsController < ApplicationController
   end
 
   def edit
-    @user = User.find_by_password_reset_token!(params[:id])
+    #@user = User.find_by_password_reset_token!(params[:id])
+    password_reset = PasswordReset.from_token(params[:id])
+    @user = password_reset.user
   end
 
   def update
-    @user = User.find_by_password_reset_token!(params[:id])
-    if @user.password_reset_expired?
+    #@user = User.find_by_password_reset_token!(params[:id])
+    #if @user.password_reset_expired?
+    password_reset = PasswordReset.from_token(params[:id])
+    @user = password_reset.user
+    if password_reset.expired?
       redirect_to new_password_reset_url, alert: "Password reset has expired."
     elsif @user.update_attributes(params[:user])
       redirect_to root_url, notice: "Password has been reset!"
