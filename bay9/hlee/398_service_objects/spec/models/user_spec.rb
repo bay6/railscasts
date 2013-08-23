a15913e5874a6faabcbaf34ef283ec1dd73c9579
Ken Lu <ken@seeice.com>
2013-08-23 08:59:48 -0400

    added rspec services for password reset, however got bug about feature function test

--- bay9/hlee/398_service_objects/spec/models/user_spec.rb
+++ bay9/hlee/398_service_objects/spec/models/user_spec.rb
@@ -2,15 +2,6 @@ require 'spec_helper'
 
 describe User do
 
-  describe '.search' do
-    it "finds users matching username or email" do
-      user = create(:user, username: "foobar", email: "foobar@example.com")
-      User.search("foobar").should eq([user])
-      User.search("foobar@example.com").should eq([user])
-      User.search("foobar2").should_not eq([user])
-    end
-  end
-
   describe '.to_csv' do
     it "returns the records in CSV format" do
       user = create(:user)
@@ -36,37 +27,6 @@ describe User do
       user.reached_invitation_limit?.should be_false
       user.invitation_count = 6
       user.reached_invitation_limit?.should be_true
-    end
-  end
-
-  describe '#send_password_reset' do
-    let(:user) { create(:user) }
-
-    it "generates a unique password_reset_token each time" do
-      user.send_password_reset
-      last_token = user.password_reset_token
-      user.send_password_reset
-      user.password_reset_token.should_not eq(last_token)
-    end
-
-    it "saves the time the password reset was sent" do
-      user.send_password_reset
-      user.reload.password_reset_sent_at.should be_present
-    end
-
-    it "delivers email to user" do
-      user.send_password_reset
-      last_email.to.should include(user.email)
-    end
-  end
-
-  describe '#password_reset_expired?' do
-    it "if sent over two hours ago" do
-      user = build(:user)
-      user.password_reset_sent_at = Time.zone.now
-      user.password_reset_expired?.should be_false
-      user.password_reset_sent_at = 3.hours.ago
-      user.password_reset_expired?.should be_true
     end
   end
 end
