c3c50b07c64c2d78922ac6af4fda2cc33a7fd4ba
Ken Lu <ken@seeice.com>
2013-08-24 16:16:15 -0400

    added before code 409

---
+++ 409-serializer/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '4bb33f46ef52ec0dbb6a72c655d351348cc81aed4b34dd312361d6fb8e2a488d9a3eb27aa503963856687af92382dc58af43cec726d9bd8944399ebcd2eb232b'
