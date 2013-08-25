c4e467b92bef5b5eb8f1500bf161371bc357902f
caok <caok1231@gmail.com>
2013-08-25 17:27:06 +0800

    init 408

---
+++ caok/408/raffler/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Raffler::Application.config.secret_key_base = 'e8e5549df2f4c316865368834b7d0125806f2da639d7dd1b1191e02cfda901fd74ceca661c5cfb698512ff2fc589ec12875a13c0de41bb03637a86970f7ec6bb'
