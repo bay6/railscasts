24ba966b882c39bde57aabdfdc7e60472c2d16e7
xiongbo <xiongbo027@gmail.com>
2013-08-26 21:38:23 +0800

    modify scope with openstruct

--- xiongbo/409-active-model-serializers/blog/app/serializers/article_serializer.rb
+++ xiongbo/409-active-model-serializers/blog/app/serializers/article_serializer.rb
@@ -3,7 +3,15 @@ class ArticleSerializer < ActiveModel::Serializer
   has_many :comments
   embed :ids, include: true
 
+  delegate :current_user, to: :scope
+
   def url
     article_url(object)
+  end
+
+  def attributes
+    data = super
+    data[:edit_url] = edit_article_url(object) if current_user.admin?
+    data
   end
 end
