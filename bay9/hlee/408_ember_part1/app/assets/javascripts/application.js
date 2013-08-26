d92046017bab08b98d987ddb461231aae332be20
Ken Lu <ken@seeice.com>
2013-08-26 09:54:57 -0400

    installed ember rails

--- bay9/hlee/408_ember_part1/app/assets/javascripts/application.js
+++ bay9/hlee/408_ember_part1/app/assets/javascripts/application.js
@@ -12,4 +12,11 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require handlebars
+//= require ember
+//= require ember-data
+//= require_self
+//= require raffler
+Raffler = Ember.Application.create();
+
 //= require_tree .
--- bay9/hlee/408_ember_part1/config/routes.rb
+++ bay9/hlee/408_ember_part1/config/routes.rb
@@ -1,4 +1,5 @@
 Raffler::Application.routes.draw do
+  root to: 'application#index'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
