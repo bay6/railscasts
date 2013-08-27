d19dda8109316c7c498bdcd72e6e3cc7444648ae
jsvisa <delweng@gmail.com>
2013-08-27 08:42:58 +0800

    finished railscast417

---
+++ jsvisa/railscasts417-foundation/store/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Store::Application.config.secret_key_base = '2b3e3dc4940e26522b9bc98b948d5ccc1a6b30d85b0c069cbd8467a1006d9e3525433dad61ea5362295e6bf772a536a0d3aeafe7488179b7c66833bfbb6377b3'
