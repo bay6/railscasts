e732782fd742328d3960cd0ae872948a5a0ff161
Ken Lee <ken@seeice.com>
2013-08-27 17:16:56 -0400

    added one more jasmine gem

--- bay9/addtionial/jasmine/practice_1/ember_jasmine/config/routes.rb
+++ bay9/addtionial/jasmine/practice_1/ember_jasmine/config/routes.rb
@@ -1,5 +1,6 @@
 Raffler::Application.routes.draw do
   root to: 'application#index'
+  mount JasmineRails::Engine => "/specs" if defined?(JasmineRails)
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
