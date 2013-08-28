ed346cb4d6d9bf44e8d3e38c178a8396a629d991
xiongbo <xiongbo027@gmail.com>
2013-08-28 14:40:30 +0800

    init

---
+++ xiongbo/165r-edit-mutiple/readme.md
@@ -0,0 +1,3 @@
+# rails casts practices --165 edit multiple
+
+## resources
---
+++ xiongbo/165r-edit-mutiple/store/app/controllers/products_controller.rb
@@ -0,0 +1,37 @@
+class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def create
+    @product = Product.new(params[:product])
+    if @product.save
+      redirect_to products_url, notice: 'Product was successfully created.'
+    else
+      render "new"
+    end
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update_attributes(params[:product])
+      redirect_to products_url, notice: 'Product was successfully updated.'
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_url, notice: "Product was successfully destroyed."
+  end
+end
