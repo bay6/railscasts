33b0f05087ead2737c6835ed4cd755ea106bebcd
soffolk <zlx.star@gmail.com>
2013-08-29 00:46:59 +0800

    complete 405 angularjs

---
+++ soffolk/405_angularjs/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Angularjs::Application.config.secret_key_base = 'd55de1efffdce89ac0c2c178f1ea41a2a144447e1cf5d845018fb10fd49ba8018e2262c01d20e4fff2483e11193d2998a0dc184b5787799c361125d9ca9ba39a'
---
+++ soffolk/405_angularjs/db/seeds.rb
@@ -0,0 +1,6 @@
+Entry.create!(name: 'Matz')
+Entry.create!(name: 'DHH')
+Entry.create!(name: 'Ryan')
+Entry.create!(name: 'Soffolk')
+Entry.create!(name: 'William')
+
