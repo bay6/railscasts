b843d1d47d11e505d9ade7c469f7213f33794b1e
William Herry <WilliamHerryChina@Gmail.com>
2013-08-30 23:12:29 +0800

    408 ember part 1

---
+++ williamherry/408-ember-part-1/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Raffler::Application.config.secret_key_base = 'e4fd097927747dd62d3e8640d6806d84e103b0068fd9a84b7050bd9e5ffcadfcb5d909bf17f1b1087401ab74fc523d802780af4cc67fbacdc1a8e1efa28e562b'
---
+++ williamherry/408-ember-part-1/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Raffler::Application.config.session_store :cookie_store, key: '_raffler_session'
