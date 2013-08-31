7fcbf52cee2d10ec6f9962f29917b5e4b3dea775
Ken Lu <ken@seeice.com>
2013-08-31 15:38:48 -0400

    respond_to json

--- bay9/hlee/409-active-model-serializers/app/controllers/articles_controller.rb
+++ bay9/hlee/409-active-model-serializers/app/controllers/articles_controller.rb
@@ -5,5 +5,9 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
+    respond_to do |format|
+      format.html
+      format.json { render json: @article }
+    end
   end
 end
