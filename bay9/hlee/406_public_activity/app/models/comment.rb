72811858f005f32fbf9d0c5f4595e6482fd1edc6
Ken Lee <ken@seeice.com>
2013-09-03 09:53:36 -0400

    show some activities

--- bay9/hlee/406_public_activity/app/models/comment.rb
+++ bay9/hlee/406_public_activity/app/models/comment.rb
@@ -1,6 +1,9 @@
 class Comment < ActiveRecord::Base
-  include PublicActivity::Model
-  tracked
+  include PublicActivity::Common
+  #tracked owner: ->(controller, model) { controller.current_user }
+  #tracked
+  #tracked owner: ->(controller, model) { controller && controller.current_user }
+  tracked except: :update, owner: ->(controller, model) { controller && controller.current_user }
   attr_accessible :content
 
   belongs_to :user
