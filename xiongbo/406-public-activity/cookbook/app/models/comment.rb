af320440ef5c42eb4d9c783baa88986408a15421
xiongbo <xiongbo027@gmail.com>
2013-09-03 14:24:30 +0800

    excluding actions by move activity tracker to the controller

--- xiongbo/406-public-activity/cookbook/app/models/comment.rb
+++ xiongbo/406-public-activity/cookbook/app/models/comment.rb
@@ -1,6 +1,5 @@
 class Comment < ActiveRecord::Base
-  include PublicActivity::Model
-  tracked owner: ->(controller, model) { controller && controller.current_user}
+  include PublicActivity::Common
 
   attr_accessible :content
 
