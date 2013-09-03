b859988885a3b84c09113b4c43e211ae73d13c1b
Ken Lee <ken@seeice.com>
2013-09-03 11:14:53 -0400

    finished 406 public activity

--- bay9/hlee/406_public_activity/app/controllers/activities_controller.rb
+++ bay9/hlee/406_public_activity/app/controllers/activities_controller.rb
@@ -1,6 +1,6 @@
 class ActivitiesController < ApplicationController
   def index
-    #@activities = PublicActivity::Activity.order('created_at desc')
-    @activities = PublicActivity::Activity.order("created_at desc").where(owner_id: current_user.friend_ids, owner_type: "User")
+    @activities = PublicActivity::Activity.order('created_at desc')
+    #@activities = PublicActivity::Activity.order("created_at desc").where(owner_id: current_user.friend_ids, owner_type: "User")
   end
 end
