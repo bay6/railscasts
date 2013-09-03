49d22771b0e95dfe528701177484aaabe793878b
xiongbo <xiongbo027@gmail.com>
2013-09-03 13:03:01 +0800

    add activities page

--- xiongbo/406-public-activity/cookbook/app/models/comment.rb
+++ xiongbo/406-public-activity/cookbook/app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ActiveRecord::Base
   include PublicActivity::Model
-  tracked owner: ->(controller, model) { controller && controller.current_user}
+  #tracked owner: ->(controller, model) { controller && controller.current_user}
 
   attr_accessible :content
 
