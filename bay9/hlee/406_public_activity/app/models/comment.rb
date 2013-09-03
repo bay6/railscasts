fc33beb6e5c4002146284e6e0f38f4ef1302c4f9
Ken Lee <ken@seeice.com>
2013-09-03 10:23:59 -0400

    still not working properly

--- bay9/hlee/406_public_activity/app/models/comment.rb
+++ bay9/hlee/406_public_activity/app/models/comment.rb
@@ -1,9 +1,5 @@
 class Comment < ActiveRecord::Base
   include PublicActivity::Common
-  #tracked owner: ->(controller, model) { controller.current_user }
-  #tracked
-  #tracked owner: ->(controller, model) { controller && controller.current_user }
-  tracked except: :update, owner: ->(controller, model) { controller && controller.current_user }
   attr_accessible :content
 
   belongs_to :user
