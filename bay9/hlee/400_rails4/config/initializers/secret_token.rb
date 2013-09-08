0f2470fef8d6ab3aff9f07885d6f0e908b2b8fcc
Ken Lu <ken@seeice.com>
2013-09-08 08:07:26 -0400

    started working on 400 rails

---
+++ bay9/hlee/400_rails4/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = '1935cf0840fcf9c16bdef07c1a5b7e622518e0368de45bbfd9845315665762b7f71ec9c9c195815e52d94a6de51616148854e85f724ca9b480562c4832ffbef6'
