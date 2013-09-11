dd8efad778c8412c8ff02239607913aeb39dea7c
caok <caok1231@gmail.com>
2013-09-11 21:49:00 +0800

    add angularjs

--- caok/405/angularjs/config/routes.rb
+++ caok/405/angularjs/config/routes.rb
@@ -1,4 +1,5 @@
 Angularjs::Application.routes.draw do
+  root "raffle#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
