8dae938db6159a36d314cdd6376296f4e3e84595
xiongbo <xiongbo027@gmail.com>
2013-09-11 11:18:11 +0800

    add actioncontroller-live and init

---
+++ xiongbo/401-actioncontroller-live/chatter/app/controllers/messages_controller.rb
@@ -0,0 +1,20 @@
+class MessagesController < ApplicationController
+  include ActionController::Live
+
+  def index
+    @messages = Message.all
+  end
+
+  def create
+    @message = Message.create!(params[:message].permit(:content, :name))
+  end
+
+  def events
+    3.times do |n|
+      response.stream.write "#{n}..\n\n"
+      sleep 2
+    end
+  ensure
+    response.stream.close
+  end
+end
