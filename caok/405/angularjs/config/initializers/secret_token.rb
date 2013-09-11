8c0509d59d7500bcc375386af0ebcd4b1032307d
caok <caok1231@gmail.com>
2013-09-11 21:31:57 +0800

    init 405

---
+++ caok/405/angularjs/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Angularjs::Application.config.secret_key_base = 'b9514dc3cd78db90404fa319b776b071cd7157b249f196e149af50f2175f1a3981e74ceb646661d10394de16672dfffffb12f9420d7262c9dac9922a48a7d0af'
