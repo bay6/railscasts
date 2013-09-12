26d962c630fda5522845469b12c6561dde7b8f42
Ken Lu <ken@seeice.com>
2013-09-12 08:25:24 -0400

    replace memcache to use redis

--- bay9/hlee/399-autocomplete-search-terms/app/models/search_suggestion.rb
+++ bay9/hlee/399-autocomplete-search-terms/app/models/search_suggestion.rb
@@ -1,12 +1,17 @@
 class SearchSuggestion < ActiveRecord::Base
   attr_accessible :popularity, :term
 
+  #def self.terms_for(prefix)
+    #Rails.cache.fetch(["search-term", prefix]) do
+      #suggestions = where("term like ?", "#{prefix}_%")
+      #suggestions.order("popularity desc").limit(10).pluck(:term)
+    #end
+  #end
+
   def self.terms_for(prefix)
-    Rails.cache.fetch(["search-term", prefix]) do
-      suggestions = where("term like ?", "#{prefix}_%")
-      suggestions.order("popularity desc").limit(10).pluck(:term)
-    end
-  end
+    $redis.zrevrange "search-suggestions:#{prefix.downcase}", 0, 9
+  end 
+
 
   def self.index_products
     Product.find_each do |product|
@@ -16,10 +21,17 @@ class SearchSuggestion < ActiveRecord::Base
     end
   end
 
+  #def self.index_term(term)
+    #where(term: term.downcase).first_or_initialize.tap do |suggestion|
+      #suggestion.increment! :popularity
+    #end
+  #end  
+
   def self.index_term(term)
-    where(term: term.downcase).first_or_initialize.tap do |suggestion|
-      suggestion.increment! :popularity
+    1.upto(term.length - 1) do |n|
+      prefix = term[0, n]
+      $redis.zincrby "search-suggestions:#{prefix.downcase}", 1, term.downcase
     end
-  end  
+  end
 
 end
