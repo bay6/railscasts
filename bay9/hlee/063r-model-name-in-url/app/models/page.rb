b9c44bebaed7c7238746a8ff7207f27bd4bfdf04
Ken Lu <ken@seeice.com>
2013-09-16 07:53:56 -0400

    routes and validate

--- bay9/hlee/063r-model-name-in-url/app/models/page.rb
+++ bay9/hlee/063r-model-name-in-url/app/models/page.rb
@@ -2,6 +2,7 @@ class Page < ActiveRecord::Base
   attr_accessible :parent_id, :content, :name
 
   validates :slug, uniqueness: true, presence: true
+           exclusion: {in: %w[signup login]}
   before_validation :generate_slug
   
   has_ancestry
