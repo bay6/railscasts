584f588d183c5d9ef3c44a564bb3f0430e56b705
xiongbo <xiongbo027@gmail.com>
2013-09-17 14:42:29 +0800

    add middle ware

--- xiongbo/399-autocomplete-serach-terms/store-before/config/application.rb
+++ xiongbo/399-autocomplete-serach-terms/store-before/config/application.rb
@@ -58,5 +58,6 @@ module Store
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    config.middleware.insert_before 0, "SearchSuggestions"
   end
 end
