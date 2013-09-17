d49dfec7d00a629052e69b052d8285b8aea2889c
xiongbo <xiongbo027@gmail.com>
2013-09-17 15:28:39 +0800

    add cache

--- xiongbo/399-autocomplete-serach-terms/store-before/app/middleware/search_suggestions.rb
+++ xiongbo/399-autocomplete-serach-terms/store-before/app/middleware/search_suggestions.rb
@@ -6,7 +6,7 @@ class SearchSuggestions
   def call(env)
     if env["PATH_INFO"] == "/search_suggestions"
       request = Rack::Request.new(env)
-      terms = SearchSuggestion.terms_for(request.params[:term])
+      terms = SearchSuggestion.terms_for(request.params["term"])
       [200, {"Content-Type" => "application/json"}, [terms.to_json]]
     else
       @app.call(env)
--- xiongbo/399-autocomplete-serach-terms/store-before/app/models/search_suggestion.rb
+++ xiongbo/399-autocomplete-serach-terms/store-before/app/models/search_suggestion.rb
@@ -1,9 +1,11 @@
 class SearchSuggestion < ActiveRecord::Base
   attr_accessible :popularity, :term
 
-  def self.terms_for prefix
-    suggestions = where("term like ?", "#{prefix}_%")
-    suggestions.order("popularity desc").limit(10).pluck(:term)
+  def self.terms_for(prefix)
+    Rails.cache.fetch(["search-terms", prefix]) do
+      suggestions = where("term like ?", "%#{prefix}%")
+      suggestions.order("popularity desc").limit(10).pluck(:term)
+    end
   end
 
   def self.index_products
@@ -14,7 +16,7 @@ class SearchSuggestion < ActiveRecord::Base
     end
   end
 
-  def self.index_term term
+  def self.index_term(term)
     where(term: term.downcase).first_or_initialize.tap do |suggestion|
       suggestion.increment! :popularity
     end
