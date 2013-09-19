3fb684feae98779c2f0a862aa83153a5c0df414a
xiongbo <xiongbo027@gmail.com>
2013-09-19 16:41:36 +0800

    add slug

--- xiongbo/063r-model-name-in-url/cms/app/models/page.rb
+++ xiongbo/063r-model-name-in-url/cms/app/models/page.rb
@@ -1,9 +1,16 @@
 class Page < ActiveRecord::Base
   attr_accessible :parent_id, :content, :name
+
+  validates :slug, uniqueness: true, presence: true
+  before_validation :generate_slug
   
   has_ancestry
 
   def to_param
-    "#{id}-#{name}".parameterize
+    slug
+  end
+
+  def generate_slug
+    self.slug ||= name.parameterize
   end
 end
