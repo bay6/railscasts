62530fdd5c74d76bdbe73b26dc1fec02a43b0c65
xiongbo <xiongbo027@gmail.com>
2013-09-23 13:10:33 +0800

    add excel support

--- xiongbo/396-importing-csv-and-excel/store-before/app/models/product.rb
+++ xiongbo/396-importing-csv-and-excel/store-before/app/models/product.rb
@@ -11,8 +11,23 @@ class Product < ActiveRecord::Base
   end
 
   def self.import(file)
-    CSV.foreach(file.path, headers: true) do |row|
-      Product.create! row.to_hash
+    spreadsheet = open_spreadsheet(file)
+    header = spreadsheet.row(1)
+
+    (2..spreadsheet.last_row).each do |i|
+      row = Hash[[header,spreadsheet.row(i)].transpose]
+      product = find_by_id(row["id"]) || new
+      product.attributes = row.to_hash.slice(*accessible_attributes)
+      product.save!
+    end
+  end
+
+  def self.open_spreadsheet(file)
+    case File.extname(file.original_filename)
+    when '.csv' then Roo::Csv.new(file.path, nil, :ignore)
+    when '.xls' then Roo::Excel.new(file.path, nil, :ignore)
+    when '.xlsx' then Roo::Excelx.new(file.path, nil, :ignore)
+    else raise "Unknown file type: #{file.original_filename}"
     end
   end
 end
