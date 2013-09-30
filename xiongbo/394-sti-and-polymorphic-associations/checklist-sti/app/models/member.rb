6df553b56dbd0e3370964e71da94b8770a0b0a10
xiongbo <xiongbo027@gmail.com>
2013-09-30 21:20:37 +0800

    add model member and guest modify user model and user controller

---
+++ xiongbo/394-sti-and-polymorphic-associations/checklist-sti/app/models/member.rb
@@ -0,0 +1,31 @@
+class Member < User
+
+  has_many :tasks, dependent: :destroy
+  attr_accessible :username, :email, :password, :password_confirmation
+
+  validates_presence_of :username, :email
+  validates_uniqueness_of :username, allow_blank: true
+  validates_confirmation_of :password
+
+  has_secure_password
+
+  def guest?
+    false
+  end
+  
+  def name
+    username
+  end
+
+  def task_limit
+    1000
+  end
+  
+  def can_share_task?(task)
+    task.user_id == id
+  end
+  
+  def send_password_reset
+    UserMailer.password_reset(self).deliver
+  end
+end
