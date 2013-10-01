1565102f5b61598439a85287dd4a6d69c9194f72
caok <caok1231@gmail.com>
2013-10-01 15:10:20 +0800

    init 229r

---
+++ caok/229r/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'e523dcc8bb6f1d8e1d425b07f33b1c6fcb00eacb9a00154b5a04e0df01e262dcfc190d16e453b6b74bf49314eac9cd84b15d8ecdb5e1bab94b33426f66fe4f31'
