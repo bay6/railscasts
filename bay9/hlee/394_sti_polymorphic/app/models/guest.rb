4c2b638cf136016d6989f3b69c29a8fb93cdb3b9
Ken Lu <ken@seeice.com>
2013-10-01 08:31:00 -0400

    added guest class to support it

---
+++ bay9/hlee/394_sti_polymorphic/app/models/guest.rb
@@ -0,0 +1,23 @@
+class Guest < User
+  def guest?
+    true
+  end
+  def move_to(user)
+    tasks.update_all(user_id: user.id)
+  end
+  
+  def name
+    "Guest"
+  end
+
+  def task_limit
+    10
+  end
+  
+  def can_share_task?(task)
+    false
+  end
+  
+  def send_password_reset
+  end
+end
