6590a2d2e349e53719f076d66d422b49d8572dfc
Ken Lu <ken@seeice.com>
2013-10-02 08:06:14 -0400

    guest methods to member and user

--- bay9/hlee/394_polymorphic/app/models/guest_profile.rb
+++ bay9/hlee/394_polymorphic/app/models/guest_profile.rb
@@ -1,4 +1,27 @@
 class GuestProfile < ActiveRecord::Base
   # attr_accessible :title, :body
   has_one :user, as: :profile, dependent: :destroy
+
+  def guest?
+    true
+  end
+  
+  def move_to(user)
+    tasks.update_all(user_id: user.id)
+  end
+
+  def name
+    "Guest"
+  end
+
+  def task_limit
+    10
+  end
+
+  def can_share_task?(task)
+    false
+  end
+
+  def send_password_reset
+  end
 end
--- bay9/hlee/394_polymorphic/app/models/member_profile.rb
+++ bay9/hlee/394_polymorphic/app/models/member_profile.rb
@@ -1,4 +1,29 @@
 class MemberProfile < ActiveRecord::Base
   has_one :user, as: :profile, dependent: :destroy
   attr_accessible :email, :password_digest, :username
+
+  validates_presence_of :username, :email
+  validates_uniqueness_of :username, allow_blank: true
+
+  has_secure_password
+
+  def guest?
+    false
+  end
+
+  def name
+    username
+  end
+
+  def task_limit
+    1000
+  end
+
+  def can_share_task?(task)
+    task.user_id == id
+  end
+
+  def send_password_reset
+    UserMailer.password_reset(self).deliver
+  end  
 end
