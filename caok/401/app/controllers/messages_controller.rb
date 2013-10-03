a9c7999327b6fb290f9a0791dcae14e7ebb1881c
caok <caok1231@gmail.com>
2013-10-03 09:48:46 +0800

    add eventlistener

--- caok/401/app/controllers/messages_controller.rb
+++ caok/401/app/controllers/messages_controller.rb
@@ -9,10 +9,19 @@ class MessagesController < ApplicationController
   end
 
   def events
-    3.times do |n|
-      response.stream.write "#{n}..\n\n"
+    response.headers["Content-Type"] = "text/event-stream"
+    start = Time.zone.now
+    10.times do
+      Message.uncached do
+        Message.where('created_at > ?', start).each do |message|
+          response.stream.write "data: #{message.to_json}\n\n"
+          start = message.created_at
+        end
+      end
       sleep 2
     end
+  rescue IOError
+    logger.info "Stream closed"
   ensure
     response.stream.close
   end
