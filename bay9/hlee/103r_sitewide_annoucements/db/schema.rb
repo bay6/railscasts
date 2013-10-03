311f300d8cf8172b570e742bcb88fb8b950c103d
Ken Lu <ken@seeice.com>
2013-10-03 15:44:39 -0400

    initial the function of announcement

--- bay9/hlee/103r_sitewide_annoucements/db/schema.rb
+++ bay9/hlee/103r_sitewide_annoucements/db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120810222509) do
+ActiveRecord::Schema.define(:version => 20131003194322) do
+
+  create_table "announcements", :force => true do |t|
+    t.text     "message"
+    t.datetime "starts_at"
+    t.datetime "ends_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "articles", :force => true do |t|
     t.string   "name"
