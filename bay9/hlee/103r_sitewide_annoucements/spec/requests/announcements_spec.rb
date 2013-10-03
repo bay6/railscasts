963d0f0081196bf1fcf36ed631da3bca76ef78a2
Ken Lu <ken@seeice.com>
2013-10-03 15:48:44 -0400

    some scope added and changed the way

--- bay9/hlee/103r_sitewide_annoucements/spec/requests/announcements_spec.rb
+++ bay9/hlee/103r_sitewide_annoucements/spec/requests/announcements_spec.rb
@@ -1,11 +1,10 @@
 require 'spec_helper'
 
-describe "Announcements" do
-  it "displays active announcements" do
-    Announcement.create! message: "Hello World", starts_at: 1.hour.ago, ends_at: 1.hour.from_now
-    Announcement.create! message: "Upcoming", starts_at: 10.minutes.from_now, ends_at: 1.hour.from_now
-    visit root_path
-    page.should have_content "Hello World"
-    page.should_not have_content "Upcoming"
+describe Announcement do
+  it "has current scope", focus: true do
+    past = Announcement.create! starts_at: 1.day.ago, ends_at: 1.hour.ago
+    current = Announcement.create! starts_at: 1.hour.ago, ends_at: 1.day.from_now
+    upcoming = Announcement.create! starts_at: 1.hour.from_now, ends_at: 1.day.from_now
+    Announcement.current.should eq([current])
   end
 end
