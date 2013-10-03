644a42ea22d5dd5f233d2ea4e32d90ab17a048db
Ken Lu <ken@seeice.com>
2013-10-03 12:41:39 -0400

    support file of connect database

--- bay9/hlee/391_testing_js_phantomjs/spec/requests/orders_spec.rb
+++ bay9/hlee/391_testing_js_phantomjs/spec/requests/orders_spec.rb
@@ -6,4 +6,10 @@ describe "Orders" do
     fill_in "Credit Card Number", with: "1234"
     page.should have_content("Invalid card number")
   end
+
+  it "fetches more orders when scrolling to the bottom", js: true do
+    11.times { |n| Order.create! number: n+1 }
+    visit orders_path
+    page.should have_content('Order #1')
+  end
 end
--- bay9/hlee/391_testing_js_phantomjs/spec/spec_helper.rb
+++ bay9/hlee/391_testing_js_phantomjs/spec/spec_helper.rb
@@ -7,6 +7,8 @@ require 'rspec/autorun'
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+require 'capybara/poltergeist'
+Capybara.javascript_driver = :poltergeist
 
 RSpec.configure do |config|
   # ## Mock Framework
