98281df241ad611c6ecbae1aa81b16801f6a87c2
Ken Lu <ken@seeice.com>
2013-10-03 11:44:20 -0400

    rspec request test

--- bay9/hlee/391_testing_js_phantomjs/spec/requests/orders_spec.rb
+++ bay9/hlee/391_testing_js_phantomjs/spec/requests/orders_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
 describe "Orders" do
-  it "validates card number"
+  it "validates card number", js: true do
+    visit new_order_path
+    fill_in "Credit Card Number", with: "1234"
+    page.should have_content("Invalid card number")
+  end
 end
