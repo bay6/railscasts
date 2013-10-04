920386f34137fe4014b2b468293ecddeaf348342
caok <caok1231@gmail.com>
2013-10-04 22:25:43 +0800

    init 162r

---
+++ caok/162r/cms/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cms::Application.config.secret_key_base = '2271349f432e792dff6c15ddcbddc0fbffaffdcc3cf6eb1754d885160fa704b790e642a0ffa4efb9fb5f0776fa6052bf1e891a07075874da48b0453148aa4320'
