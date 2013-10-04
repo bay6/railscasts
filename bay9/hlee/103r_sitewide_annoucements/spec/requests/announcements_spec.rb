ad4e3339145149bf0865922e06db07c1ff287660
Ken Lu <ken@seeice.com>
2013-10-04 08:05:43 -0400

    new routes

--- bay9/hlee/103r_sitewide_annoucements/spec/requests/announcements_spec.rb
+++ bay9/hlee/103r_sitewide_annoucements/spec/requests/announcements_spec.rb
@@ -7,4 +7,15 @@ describe Announcement do
     upcoming = Announcement.create! starts_at: 1.hour.from_now, ends_at: 1.day.from_now
     Announcement.current.should eq([current])
   end
+
+
+  it "displays active announcements" do
+    Announcement.create! message: "Hello World", starts_at: 1.hour.ago, ends_at: 1.hour.from_now
+    Announcement.create! message: "Upcoming", starts_at: 10.minutes.from_now, ends_at: 1.hour.from_now
+    visit root_path
+    page.should have_content "Hello World"
+    page.should_not have_content "Upcoming"
+    click_on "hide announcement"
+    page.should_not have_content "Hello World"
+  end
 end
