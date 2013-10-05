07dd491cf5f286b2809c10a586500d943e1f267d
caok <caok1231@gmail.com>
2013-10-05 08:16:23 +0800

    add guest

--- caok/393/app/models/user.rb
+++ caok/393/app/models/user.rb
@@ -3,8 +3,20 @@ class User < ActiveRecord::Base
 
   attr_accessible :username, :email, :password, :password_confirmation
 
-  validates_presence_of :username, :email
-  validates_uniqueness_of :username
+  validates_presence_of :username, :email, :password_digest, unless: :guest?
+  validates_uniqueness_of :username, allow_blank: true
+  validates_confirmation_of :password
 
-  has_secure_password
+  # override has_secure_password to customize validation until Rails 4.
+  require 'bcrypt'
+  attr_reader :password
+  include ActiveModel::SecurePassword::InstanceMethodsOnActivation
+
+  def self.new_guest
+    new { |u| u.guest = true }
+  end
+
+  def name
+    guest ? "Guest" : username
+  end
 end
