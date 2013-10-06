68a24ca7c1a0fead1d1f0def22f7444cfd659cf5
xiongbo <xiongbo027@gmail.com>
2013-10-06 17:57:41 +0800

    add test and current scope

--- xiongbo/103r-site-wide-announcements/blog/spec/requests/announcements_spec.rb
+++ xiongbo/103r-site-wide-announcements/blog/spec/requests/announcements_spec.rb
@@ -2,9 +2,20 @@ require 'spec_helper'
 
 describe "Announcements" do
   it "displays active announcements" do
-    Announcements.create! message: "hello world",
+    Announcement.create! message: "hello world",
                           starts_at: 1.hour.ago, ends_at: 1.hour.from_now
+    Announcement.create! message: "upcoming",
+                          starts_at: 10.minutes.from_now,  ends_at: 1.hour.from_now
     visit root_path
-    page.should have_content "hello world"
+    expect(page).to have_content "hello world"
+    expect(page).not_to have_content "upcoming"
+  end
+
+  it "has current scope" do
+    past = Announcement.create! starts_at: 1.days.ago, ends_at: 1.hour.ago
+    current = Announcement.create! starts_at: 1.hour.ago, ends_at: 1.day.from_now
+    upcoming = Announcement.create! starts_at: 1.hour.from_now, ends_at: 1.day.from_now
+
+    Announcement.current.should eq [current]
   end
 end
