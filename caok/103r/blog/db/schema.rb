b66e6cf9177293e3b2fc5d010886853df45aa6bd
caok <caok1231@gmail.com>
2013-10-07 21:38:14 +0800

    add announcement

--- caok/103r/blog/db/schema.rb
+++ caok/103r/blog/db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120810222509) do
+ActiveRecord::Schema.define(:version => 20131007132715) do
+
+  create_table "announcements", :force => true do |t|
+    t.text     "message"
+    t.datetime "starts_at"
+    t.datetime "ends_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "articles", :force => true do |t|
     t.string   "name"
