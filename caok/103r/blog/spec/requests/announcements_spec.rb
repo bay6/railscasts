4467705edd499818056a31ef2950f21c3279c29c
caok <caok1231@gmail.com>
2013-10-07 21:57:43 +0800

    add current in announcements

--- caok/103r/blog/spec/requests/announcements_spec.rb
+++ caok/103r/blog/spec/requests/announcements_spec.rb
@@ -4,8 +4,12 @@ describe "Announcements" do
   describe "GET /announcements" do
     it "displays active announcements" do
       Announcement.create! message: "Hello World", starts_at: 1.hour.ago, ends_at: 1.hour.from_now
+      Announcement.create! message: "Upcoming", starts_at: 10.minutes.from_now, ends_at: 1.hour.from_now
       visit root_path
       page.should have_content("Hello World")
+      page.should_not have_content("Upcoming")
+      click_on "hide announcement"
+      page.should_not have_content("Hello World")
     end
   end
 end
