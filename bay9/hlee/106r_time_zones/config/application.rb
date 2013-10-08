a6ec37ad875888b4de19c2e04e6480f636e18d34
Ken Lu <ken@seeice.com>
2013-10-08 08:31:10 -0400

    updated the time zone configuration

--- bay9/hlee/106r_time_zones/config/application.rb
+++ bay9/hlee/106r_time_zones/config/application.rb
@@ -58,5 +58,6 @@ module Youhaiku
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    config.time_zone = 'Eastern Time (US & Canada)'
   end
 end
