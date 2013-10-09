f56d73186011d65ddd1846793f6c6b68a687fc1c
xiongbo <xiongbo027@gmail.com>
2013-10-09 16:35:31 +0800

    add user's time_zone set

--- xiongbo/106r-time-zones/youhaiku/app/controllers/application_controller.rb
+++ xiongbo/106r-time-zones/youhaiku/app/controllers/application_controller.rb
@@ -1,10 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  around_filter :user_time_zone, if: :current_user
+
 private
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
   helper_method :current_user
+
+  def user_time_zone(&block)
+    Time.use_zone(current_user.time_zone, &block)
+  end
 end
