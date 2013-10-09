b811315d581e669a4629fb3bb86fcb4c8b9f19c2
xiongbo <xiongbo027@gmail.com>
2013-10-09 16:29:42 +0800

    add timezone and china_zone for select

--- xiongbo/106r-time-zones/youhaiku/app/models/user.rb
+++ xiongbo/106r-time-zones/youhaiku/app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ActiveRecord::Base
   has_secure_password
 
-  attr_accessible :name, :password, :password_confirmation
+  attr_accessible :name, :password, :password_confirmation, :time_zone
 
   validates_uniqueness_of :name
+  validates_inclusion_of :time_zone, in: ActiveSupport::TimeZone.zones_map(&:name)
 
   has_many :haikus
 end
