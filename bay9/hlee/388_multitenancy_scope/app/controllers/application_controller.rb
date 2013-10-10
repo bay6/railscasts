603c7b1bc49a637cc28c9a548279e97074e24ded
Ken Lu <ken@seeice.com>
2013-10-10 18:57:55 -0400

    some subdomain issue

--- bay9/hlee/388_multitenancy_scope/app/controllers/application_controller.rb
+++ bay9/hlee/388_multitenancy_scope/app/controllers/application_controller.rb
@@ -7,4 +7,10 @@ private
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
   helper_method :current_user
+
+  def current_tenant
+    Tenant.find_by_subdomain! request.subdomain
+  end
+  helper_method :current_tenant
+
 end
