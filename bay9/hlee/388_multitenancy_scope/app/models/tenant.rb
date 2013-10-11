f39f2a9a45063d7b87ee645a024d1b801d781031
Ken Lu <ken@seeice.com>
2013-10-10 21:18:21 -0400

    skip 388 nultitenancy scope

--- bay9/hlee/388_multitenancy_scope/app/models/tenant.rb
+++ bay9/hlee/388_multitenancy_scope/app/models/tenant.rb
@@ -1,4 +1,12 @@
 class Tenant < ActiveRecord::Base
   attr_accessible :name, :subdomain
   has_many :topics
+  
+  def self.current_id=(id)
+    Thread.current[:tenant_id] = id
+  end
+  
+  def self.current_id
+    Thread.current[:tenant_id]
+  end
 end
