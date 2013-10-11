d08aa626ddbd6b988f86970fed13fc8f8cb13783
Ken Lu <ken@seeice.com>
2013-10-11 09:23:05 -0400

    authorize and limit the post function to admin

--- bay9/hlee/385_authorization_from_scratch/app/controllers/topics_controller.rb
+++ bay9/hlee/385_authorization_from_scratch/app/controllers/topics_controller.rb
@@ -1,4 +1,19 @@
 class TopicsController < ApplicationController
+  before_filter :authorize, only: :edit
+
+  # Other methods omitted
+
+  private
+  def authorize
+    if current_user && !current_user.admin?
+      redirect_to root_url, alert: "Not authorized"
+    end
+  end
+end
+
+class TopicsController < ApplicationController
+  before_filter :authorize, only: :edit
+
   def index
     @topics = Topic.order("sticky desc")
   end
@@ -41,6 +56,11 @@ class TopicsController < ApplicationController
   end
 
 private
+  def authorize
+    if current_user && !current_user.admin?
+      redirect_to root_url, alert: "Not authorized"
+    end
+  end
 
   def topic_params
     params.require(:topic).permit(:name, :sticky)
--- bay9/hlee/385_authorization_from_scratch/spec/requests/topics_spec.rb
+++ bay9/hlee/385_authorization_from_scratch/spec/requests/topics_spec.rb
@@ -1,6 +1,13 @@
 require 'spec_helper'
 
 describe "Topic request" do
+  it "cannot edit topic as non-admin" do
+    log_in admin: false
+    topic = create(:topic)
+    visit edit_topic_path(topic)
+    page.should have_content("Not authorized")
+  end
+
   it "lists topics as guest" do
     create(:topic, name: "Hello")
     create(:topic, name: "World")
