2a69e63a66554b4aeb0938d675ca7f5cd72ea152
xiongbo <xiongbo027@gmail.com>
2013-10-12 14:18:31 +0800

    fixed validates and thread safe problem

--- xiongbo/388-multitenancy-with-scopes/forum/app/models/tenant.rb
+++ xiongbo/388-multitenancy-with-scopes/forum/app/models/tenant.rb
@@ -1,4 +1,11 @@
 class Tenant < ActiveRecord::Base
   attr_accessible :name, :subdomain 
-  cattr_accessor :current_id
+
+  def self.current_id=(id)
+    Thread.current[:tenant_id] = id
+  end
+
+  def self.current_id
+    Thread.current[:tenant_id]
+  end
 end
