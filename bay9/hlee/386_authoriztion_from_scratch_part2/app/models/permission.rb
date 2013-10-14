7325d486f3051012abb90167ba7f8aeb530e705b
Ken Lu <ken@seeice.com>
2013-10-14 09:07:30 -0400

    update permission allow function

--- bay9/hlee/386_authoriztion_from_scratch_part2/app/models/permission.rb
+++ bay9/hlee/386_authoriztion_from_scratch_part2/app/models/permission.rb
@@ -1,13 +1,25 @@
 class Permission < Struct.new(:user)
-  def allow?(controller, action)
-    return true if controller == "sessions"
-    return true if controller == "users" && action.in?(%w[new create])
-    return true if controller == "topics" && action.in?(%w[index show])
+  def initialize(user)
+    allow :users, [:new, :create]
+    allow :sessions, [:new, :create, :destroy]
+    allow :topics, [:index, :show]
     if user
-      return true if controller == "users" && action.in?(%w[edit update])
-      return true if controller == "topics" && action != "destroy"
-      return true if user.admin?
+      allow :users, [:edit, :update]
+      allow :topics, [:new, :create, :edit, :update]
+      allow_all if user.admin?
     end
-    false
+  end
+
+  def allow_all
+    @allow_all = true
+  end
+
+  def allow(controllers, actions)
+    @allowed_actions ||= {}
+    Array(controllers).each do |controller|
+      Array(actions).each do |action|
+        @allowed_actions[[controller.to_s, action.to_s]] = true
+      end
+    end
   end
 end
