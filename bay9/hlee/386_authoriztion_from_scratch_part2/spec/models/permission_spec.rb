c670437f3069c9117bc513fb2d465330ec8919b6
Ken Lu <ken@seeice.com>
2013-10-17 11:11:19 -0400

    adjust the permission

--- bay9/hlee/386_authoriztion_from_scratch_part2/spec/models/permission_spec.rb
+++ bay9/hlee/386_authoriztion_from_scratch_part2/spec/models/permission_spec.rb
@@ -9,7 +9,7 @@ end
 describe Permission do
   describe "as guest" do
     subject { Permission.new(nil) }
-    
+
     it { should allow("topics", "index") }
     it { should allow("topics", "show") }
     it { should_not allow("topics", "new") }
@@ -27,31 +27,31 @@ describe Permission do
     it { should_not allow("users", "edit") }
     it { should_not allow("users", "update") }
   end
-  
+
   describe "as admin" do
     subject { Permission.new(build(:user, admin: true)) }
-    
+
     it { should allow("anything", "here") }
   end
-  
-  describe "as member" do
-    subject { Permission.new(build(:user, admin: false)) }
-    
-    it { should allow("topics", "index") }
-    it { should allow("topics", "show") }
-    it { should allow("topics", "new") }
-    it { should allow("topics", "create") }
-    it { should allow("topics", "edit") }
-    it { should allow("topics", "update") }
-    it { should_not allow("topics", "destroy") }
 
-    it { should allow("sessions", "new") }
-    it { should allow("sessions", "create") }
-    it { should allow("sessions", "destroy") }
+  describe Permission do
+    # Specs for guests and admins omitted.
 
-    it { should allow("users", "new") }
-    it { should allow("users", "create") }
-    it { should allow("users", "edit") }
-    it { should allow("users", "update") }
+    describe "as member" do
+      let(:user) { create(:user, admin: false) }
+      let(:user_topic) { build(:topic, user: user) }
+      let(:other_topic) { build(:topic) }
+
+      subject { Permission.new(user) }
+
+      it { should_not allow("topics", "edit") }
+      it { should_not allow("topics", "update") }
+      it { should_not allow("topics", "edit", other_topic) }
+      it { should_not allow("topics", "update", other_topic) }
+      it { should allow("topics", "edit", user_topic) }
+      it { should allow("topics", "update", user_topic) }
+
+      # Specs for other actions omitted.
+    end
   end
 end
