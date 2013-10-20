2e43b75f97f60c8d3fdb12444266d7b4955f20e5
caok <caok1231@gmail.com>
2013-10-20 12:26:44 +0800

    add mailer_host

--- caok/085r/config/application.rb
+++ caok/085r/config/application.rb
@@ -60,5 +60,6 @@ module Blog
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    config.action_mailer.default_url_options = {host: ENV["MAILER_HOST"]}
   end
 end
--- caok/085r/config/environments/test.rb
+++ caok/085r/config/environments/test.rb
@@ -34,6 +34,4 @@ Blog::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
-
-  config.action_mailer.default_url_options = {host: "test.local"}
 end
