c2d94868477a382202d562336680d20ceb00ad5c
caok <caok1231@gmail.com>
2013-10-20 12:36:54 +0800

    finish 85r

--- caok/085r/config/application.rb
+++ caok/085r/config/application.rb
@@ -9,11 +9,9 @@ if defined?(Bundler)
   # Bundler.require(:default, :assets, Rails.env)
 end
 
-config = YAML.load(File.read(File.expand_path('../application.yml', __FILE__)))
-config.merge! config.fetch(Rails.env, {})
-config.each do |key, value|
-  ENV[key] = value.to_s unless value.kind_of? Hash
-end
+CONFIG = YAML.load(File.read(File.expand_path('../application.yml', __FILE__)))
+CONFIG.merge! CONFIG.fetch(Rails.env, {})
+CONFIG.symbolize_keys!
 
 module Blog
   class Application < Rails::Application
