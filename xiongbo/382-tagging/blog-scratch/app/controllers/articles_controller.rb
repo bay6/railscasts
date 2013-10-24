032dc76e601ca37931477a0d82b822f5730fac9a
xiongbo <xiongbo027@gmail.com>
2013-10-24 15:35:48 +0800

    add tag association methods

--- xiongbo/382-tagging/blog-scratch/app/controllers/articles_controller.rb
+++ xiongbo/382-tagging/blog-scratch/app/controllers/articles_controller.rb
@@ -1,6 +1,10 @@
 class ArticlesController < ApplicationController
   def index
-    @articles = Article.all
+    if params[:tag]
+      @articles = Article.tagged_with params[:tag]
+    else
+      @articles = Article.all
+    end
   end
 
   def show
--- xiongbo/382-tagging/blog-scratch/app/models/article.rb
+++ xiongbo/382-tagging/blog-scratch/app/models/article.rb
@@ -2,4 +2,25 @@ class Article < ActiveRecord::Base
   attr_accessible :content, :name, :tag_list
   has_many :taggings
   has_many :tags, through: :taggings
+
+  class << self
+    def tagged_with name
+      Tag.find_by_name!(name).articles
+    end
+
+    def tag_counts
+      Tag.select("tags.*, count(taggings.tag_id) as count").
+        joins(:taggings).group("taggings.tag_id")
+    end
+  end
+
+  def tag_list
+    tags.map(&:name).join(", ")
+  end
+
+  def tag_list=(names)
+    self.tags = names.split(',').map do |name|
+      Tag.where(name: name.strip).first_or_create!
+    end
+  end
 end
