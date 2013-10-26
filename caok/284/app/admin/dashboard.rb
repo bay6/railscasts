a490d1b1346d3b017d23b15ec60398f1156c11a7
caok <caok1231@gmail.com>
2013-10-26 22:11:58 +0800

    finish 284

--- caok/284/app/admin/dashboard.rb
+++ caok/284/app/admin/dashboard.rb
@@ -10,6 +10,20 @@ ActiveAdmin.register_page "Dashboard" do
       end
     end
 
+    columns do
+      column do
+        panel "Recent Products" do
+          table_for Product.order("released_at desc").limit(5) do
+            column :name do |product|
+              link_to product.name, admin_product_path(product)
+            end
+            column :released_at
+          end
+          strong { link_to "View All Products", admin_products_path }
+        end
+      end
+    end
+
     # Here is an example of a simple dashboard with columns and panels.
     #
     # columns do
