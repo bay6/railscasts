8ccd35bb77fc00fc8bc6e8f139ab653678a33559
xiongbo <xiongbo027@gmail.com>
2013-10-28 15:01:31 +0800

    improved query

--- xiongbo/223-charts-graphs/store/app/helpers/orders_helper.rb
+++ xiongbo/223-charts-graphs/store/app/helpers/orders_helper.rb
@@ -1,9 +1,10 @@
 module OrdersHelper
   def orders_chart_data
+    orders_by_day = Order.total_grouped_by_day(2.weeks.ago)
     (2.weeks.ago.to_date..Date.today).map do |date|
       {
         purchased_at: date,
-        price: Order.where("date(purchased_at) = ?", date).sum(:price)
+        price: orders_by_day(date).first.try(:total_price) || 0
       }
     end
   end
