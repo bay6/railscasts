b2ac870247d27e57fc6b36a1ff922acd85e22d07
Ken Lu <ken@seeice.com>
2013-11-06 09:32:44 -0500

    finished 383 uploading to amazon s3

--- bay9/hlee/383_uploading_amazon_s3/app/models/painting.rb
+++ bay9/hlee/383_uploading_amazon_s3/app/models/painting.rb
@@ -14,12 +14,12 @@ class Painting < ActiveRecord::Base
 
   class ImageWorker
     include Sidekiq::Worker
-    
     def perform(id, key)
       painting = Painting.find(id)
       painting.key = key
       painting.remote_image_url = painting.image.direct_fog_url(with_path: true)
       painting.save!
+      painting.update_column(:image_processed, true)
     end
   end
 end
