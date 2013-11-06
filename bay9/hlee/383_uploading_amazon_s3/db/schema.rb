57ad0879ed3cab88f1e649a0367a9d0b736bc68c
Ken Lu <ken@seeice.com>
2013-11-06 09:29:42 -0500

    started to adding processing bar

--- bay9/hlee/383_uploading_amazon_s3/db/schema.rb
+++ bay9/hlee/383_uploading_amazon_s3/db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120919225024) do
+ActiveRecord::Schema.define(:version => 20131106142910) do
 
   create_table "paintings", :force => true do |t|
     t.string   "name"
     t.string   "image"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+    t.boolean  "image_processed"
   end
 
 end
