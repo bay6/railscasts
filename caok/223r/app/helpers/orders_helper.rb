323a3d96ead733deebd9120aa9f7bdd88dac4671
caok <caok1231@gmail.com>
2013-11-08 23:39:19 +0800

    add total_grouped_by_day

--- caok/223r/app/helpers/orders_helper.rb
+++ caok/223r/app/helpers/orders_helper.rb
@@ -1,9 +1,10 @@
 module OrdersHelper
   def orders_chart_data
+    orders_by_day = Order.total_grouped_by_day(3.weeks.ago)
     (3.weeks.ago.to_date..Date.today).map do |date|
       {
         purchased_at: date,
-        price: Order.where("date(purchased_at) = ?", date).sum(:price)
+        price: orders_by_day[date].first.try(:total_price) || 0
       }
     end
   end
