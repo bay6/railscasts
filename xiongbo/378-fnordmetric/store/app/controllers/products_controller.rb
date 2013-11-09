b0f91162086b4cfcac8ac983dee3967b2d3e8798
xiongbo <xiongbo027@gmail.com>
2013-11-09 21:30:19 +0800

    add fnordmetric to app

---
+++ xiongbo/378-fnordmetric/store/app/controllers/products_controller.rb
@@ -0,0 +1,10 @@
+class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+
+  def show
+    @product = Product.find(params[:id])
+    FNORD_METRIC.event @product.attributes.merge(_type: :view_product)
+  end
+end
