d139891b4be96636392819bd54f2b88d546832ae
xiongbo <xiongbo027@gmail.com>
2013-11-09 21:48:17 +0800

    add fnordmetirc task

--- xiongbo/378-fnordmetric/store/app/models/product.rb
+++ xiongbo/378-fnordmetric/store/app/models/product.rb
@@ -1,3 +1,7 @@
 class Product < ActiveRecord::Base
   attr_accessible :name, :price, :released_on
+
+  def trigger_view_event
+    FNORD_METRIC.event attributes.merge(_type: :view_product) 
+  end
 end
