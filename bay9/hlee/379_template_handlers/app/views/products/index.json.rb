5f8ce50379d9f104d3655798f93c8dfa188b5feb
Ken Lu <ken@seeice.com>
2013-11-12 09:07:44 -0500

    embed json working

--- bay9/hlee/379_template_handlers/app/views/products/index.json.rb
+++ bay9/hlee/379_template_handlers/app/views/products/index.json.rb
@@ -2,6 +2,11 @@
   {
     name: product.name,
     price: number_to_currency(product.price),
-    url: product_url(product)
+    url: product_url(product),
+    reviews: product.reviews.map do |r|
+      {
+        name: r.name
+      }
+    end
   }
 end.to_json
