e61c2d37b5012576c282b0372f11282a2cd13cd6
caok <caok1231@gmail.com>
2013-11-12 08:10:19 +0800

    finish 136r

--- caok/136r/app/assets/javascripts/tasks.js
+++ caok/136r/app/assets/javascripts/tasks.js
@@ -1,6 +1,11 @@
-$(function() {
-  $('.edit_task input[type=submit]').remove();
-  $('.edit_task input[type=checkbox]').click(function() {
+jQuery.fn.submitOnCheck = function() {
+  this.find('input[type=submit]').remove();
+  this.find('input[type=checkbox]').click(function() {
     $(this).parent('form').submit();
   });
+  return this;
+}
+
+$(function() {
+  $('.edit_task').submitOnCheck();
 });
