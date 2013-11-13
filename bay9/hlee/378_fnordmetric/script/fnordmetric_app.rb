90550ff598338dc6fbab0fdccbba99c17c3b88cd
Ken Lu <ken@seeice.com>
2013-11-13 09:13:22 -0500

    user and view product

--- bay9/hlee/378_fnordmetric/script/fnordmetric_app.rb
+++ bay9/hlee/378_fnordmetric/script/fnordmetric_app.rb
@@ -1,10 +1,16 @@
 require "fnordmetric"
 
 FnordMetric.namespace :store do
+  hide_active_users
+  
   toplist_gauge :popular_products, title: "Popular Products"
+  
+  distribution_gauge :popular_prices, title: "Popular Prices",
+    value_ranges: [0..5, 5..10, 10..20, 20..50, 50..10000]
   
   event :view_product do
     observe :popular_products, data[:name]
+    observe :popular_prices, data[:price]
   end
 end
 
