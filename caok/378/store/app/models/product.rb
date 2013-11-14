242700cc2570eb1bb02da262f59f86ad441c986e
caok <caok1231@gmail.com>
2013-11-14 08:02:17 +0800

    add rake fnordmetric

--- caok/378/store/app/models/product.rb
+++ caok/378/store/app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ActiveRecord::Base
   attr_accessible :name, :price, :released_on, :category_id
   belongs_to :category
+
+  def trigger_view_event
+    FNORD_METRIC.event(attributes.merge(type: :view_product))
+  end
 end
