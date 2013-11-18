76cc0672a4f510a485543cd472dd1e92ee90868f
Ken Lu <ken@seeice.com>
2013-11-18 08:32:37 -0500

    modified the show page

--- bay9/hlee/117r_semi_static_pages/db/schema.rb
+++ bay9/hlee/117r_semi_static_pages/db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120902185114) do
+ActiveRecord::Schema.define(:version => 20131118132851) do
+
+  create_table "pages", :force => true do |t|
+    t.string   "name"
+    t.string   "permalink"
+    t.text     "content"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "pages", ["permalink"], :name => "index_pages_on_permalink"
 
   create_table "products", :force => true do |t|
     t.string   "name"
