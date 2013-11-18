84fc358a2ec728e811b0b0be573686d146bfcf33
Ken Lu <ken@seeice.com>
2013-11-18 08:28:35 -0500

    changed teh route to reuseable

--- bay9/hlee/117r_semi_static_pages/config/routes.rb
+++ bay9/hlee/117r_semi_static_pages/config/routes.rb
@@ -1,10 +1,11 @@
 Store::Application.routes.draw do
   #get "info/about"
-  get 'about', to: 'info#about'
-
-  get "info/privacy"
-
-  get "info/license"
+  #get 'about', to: 'info#about'
+  #get "info/privacy"
+  #get "info/license"
+  %w[about privacy license].each do |page|
+    get page, controller: 'info', action: page
+  end
 
   resources :products
   root to: 'products#index'
