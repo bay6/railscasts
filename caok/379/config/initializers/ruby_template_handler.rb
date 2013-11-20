cc924589bb248724cc4595cc12bf48959803de80
caok <caok1231@gmail.com>
2013-11-20 23:12:58 +0800

    modify ruby_template_handler to show json.rb

--- caok/379/config/initializers/ruby_template_handler.rb
+++ caok/379/config/initializers/ruby_template_handler.rb
@@ -1,2 +1,2 @@
-handler = ->(template) { "Date.today.to_s" }
-ActionView::Template.register_template_handler(:rb, handler)
+#handler = ->(template) { template.source }
+ActionView::Template.register_template_handler(:rb, :source.to_proc)
