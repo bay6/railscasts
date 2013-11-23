b8b5cfc4e66b039269d751e564fe494b232d92ac
Ken Lu <ken@seeice.com>
2013-11-23 14:15:18 -0500

    memo with parameter

--- bay9/hlee/137r_memoization/app/models/product.rb
+++ bay9/hlee/137r_memoization/app/models/product.rb
@@ -1,12 +1,12 @@
 class Product < ActiveRecord::Base
-  def filesize
-    @filesize = calculate_filesize unless defined? @filesize
-    @filesize
+  def filesize(*args)
+    @filesize ||= calculate_filesize(*args)
   end
-
+  
 private
-  def calculate_filesize
+  def calculate_filesize(num = 1)
     sleep 0.5
-    4815162342
+    4815162342 * num
+    false
   end
 end
