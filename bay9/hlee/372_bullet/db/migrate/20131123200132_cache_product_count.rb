9a1ddca6303a6c185d6f0ee637757d25645f1562
Ken Lu <ken@seeice.com>
2013-11-23 15:04:31 -0500

    Finished 372 bullet about n plus 1 and counter cache

--- bay9/hlee/372_bullet/db/migrate/20131123200132_cache_product_count.rb
+++ bay9/hlee/372_bullet/db/migrate/20131123200132_cache_product_count.rb
@@ -1,5 +1,6 @@
 class CacheProductCount < ActiveRecord::Migration
   def up
+    execute "update categories set products_count=(select count(*) from products where category_id=categories.id)"
   end
 
   def down
