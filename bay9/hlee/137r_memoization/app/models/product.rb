59b2dccfc3398c099fa025f5a3c174d7d422a431
Ken Lu <ken@seeice.com>
2013-11-23 14:16:10 -0500

    cached parameter

--- bay9/hlee/137r_memoization/app/models/product.rb
+++ bay9/hlee/137r_memoization/app/models/product.rb
@@ -1,9 +1,10 @@
 class Product < ActiveRecord::Base
   def filesize(*args)
-    @filesize ||= calculate_filesize(*args)
+    @filesize ||= {} 
+    @filesize[args] ||= calculate_filesize(*args) 
   end
-  
-private
+
+  private
   def calculate_filesize(num = 1)
     sleep 0.5
     4815162342 * num
