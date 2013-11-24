861f565a35531362949b4af36da23f5766912239
xiongbo <xiongbo027@gmail.com>
2013-11-24 12:34:49 +0800

    add more general solution

--- xiongbo/137r-memoization/store/app/models/product.rb
+++ xiongbo/137r-memoization/store/app/models/product.rb
@@ -1,12 +1,19 @@
 class Product < ActiveRecord::Base
-  def filesize(*args)
-    @filesize ||= {}
-    @filesize[args] ||= calculate_filesize(*args)
+  def self.memoize name
+    alias_method "_unmemoized_#{name}", name
+    define_method(name) do |*args|
+      @_memoized ||= {}
+      @_memoized[name] ||= Hash.new do |hash, key|
+        hash[key] = send("_unmemoized_#{name}", *key)
+      end
+      @_memoized[name][args]
+    end
   end
 
-private
-  def calculate_filesize(num = 1)
+  def filesize(num = 1)
     sleep 0.5
-    4815162342 * num
+    4823232 * num
   end
+
+  memoize :filesize
 end
