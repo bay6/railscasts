89115b9ff768ae74e6ae47a812561cf4eb81eeb9
Ken Lu <ken@seeice.com>
2013-11-24 22:29:14 -0500

    view.captures function

--- bay9/hlee/213r_calendars/app/helpers/calendar_helper.rb
+++ bay9/hlee/213r_calendars/app/helpers/calendar_helper.rb
@@ -8,5 +8,30 @@ module CalendarHelper
     START_DAY = :sunday
     
     delegate :content_tag, to: :view
+
+    def table
+      content_tag :table, class: 'calendar' do 
+        header + week_rows
+      end
+    end
+
+    def header
+      content_tag :tr do
+        HEADER.map { |day| content_tag :th, day }.join.html_safe
+      end
+    end
+
+    def week_rows
+      weeks.map do |week|
+        content_tag :tr do
+          week.map { |day| day_cell(day) }.join.html_safe
+        end.join.html_safe
+      end
+    end
+
+    def day_cell(day)
+      content_tag :td, view.captures(day, &callback), class: day_classes(day)
+    end
+
   end
 end
