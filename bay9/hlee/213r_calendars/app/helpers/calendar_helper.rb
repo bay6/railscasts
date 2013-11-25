fc9950611cae67c92cb205f65e2811b9905200e9
Ken Lu <ken@seeice.com>
2013-11-24 22:14:57 -0500

    calendar class and delegate function

--- bay9/hlee/213r_calendars/app/helpers/calendar_helper.rb
+++ bay9/hlee/213r_calendars/app/helpers/calendar_helper.rb
@@ -3,5 +3,10 @@ module CalendarHelper
     Calendar.new(self, date, block).table
   end
 
-  
+  class Calendar < Struct.new(:view, :date, :callback)
+    HEADER = %w(Sunday Monday Tuesday Wednesday Thursday Friday Saturday)
+    START_DAY = :sunday
+    
+    delegate :content_tag, to: :view
+  end
 end
