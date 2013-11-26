be4b2fda9d5c72b4ddcc8dbeb00d4403f5f898be
xiongbo <xiongbo027@gmail.com>
2013-11-26 21:46:37 +0800

    add calendar helper

---
+++ xiongbo/213r-calendars/blog/app/helpers/calendar_helper.rb
@@ -0,0 +1,8 @@
+module CalendarHelper
+  def calendar(date = Date.today, &block)
+    Calendar.new(self, date, block).table
+  end
+
+  class Calendar < Struct.new(:view, :date, :callback)
+  end
+end
