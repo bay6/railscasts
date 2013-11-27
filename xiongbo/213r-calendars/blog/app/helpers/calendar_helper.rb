9b7852648ae53af036f3f33808c286473dde968f
xiongbo <xiongbo027@gmail.com>
2013-11-27 11:20:04 +0800

    add calendar Class to output calendar

--- xiongbo/213r-calendars/blog/app/helpers/calendar_helper.rb
+++ xiongbo/213r-calendars/blog/app/helpers/calendar_helper.rb
@@ -1,8 +1,2 @@
 module CalendarHelper
-  def calendar(date = Date.today, &block)
-    Calendar.new(self, date, block).table
-  end
-
-  class Calendar < Struct.new(:view, :date, :callback)
-  end
 end
