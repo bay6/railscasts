c4cac90d42b85c5ccb75393f3f548f61189b1e2b
xiongbo <xiongbo027@gmail.com>
2013-11-27 11:36:58 +0800

    add prev and next month link

--- xiongbo/213r-calendars/blog/app/controllers/articles_controller.rb
+++ xiongbo/213r-calendars/blog/app/controllers/articles_controller.rb
@@ -2,6 +2,7 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
     @articles_by_date = @articles.group_by(&:published_on)
+    @date = params[:date] ? Date.parse(params[:date]) : Date.today
   end
 
   def show
