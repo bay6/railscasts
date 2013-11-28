66bbcf133cfe2a680951762d807ba97e3ff10fda
xiongbo <xiongbo027@gmail.com>
2013-11-28 19:54:04 +0800

    use bullet to detect N+1 problem

--- xiongbo/372-bullet/store/config/environments/development.rb
+++ xiongbo/372-bullet/store/config/environments/development.rb
@@ -34,4 +34,9 @@ Store::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  config.after_initialize do
+    Bullet.enable = true
+    Bullet.alert = true
+  end
 end
