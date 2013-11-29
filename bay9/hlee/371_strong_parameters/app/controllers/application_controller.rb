19327c875b5d3606519b5b9663095159778461a0
Ken Lu <ken@seeice.com>
2013-11-29 08:49:09 -0500

    move the permit to a seperate model

--- bay9/hlee/371_strong_parameters/app/controllers/application_controller.rb
+++ bay9/hlee/371_strong_parameters/app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+def permitted_params
+  @permitted_params ||= PermittedParams.new(params, current_user)
+end
+
+
 private
 
   def current_user
