278533474e2a240b57b126620b06df32a3983b98
xiongbo <xiongbo027@gmail.com>
2013-12-01 18:08:33 +0800

    add lock_version to _form and model

--- xiongbo/59r-optimistic-locking/store/app/models/product.rb
+++ xiongbo/59r-optimistic-locking/store/app/models/product.rb
@@ -1,4 +1,4 @@
 class Product < ActiveRecord::Base
   belongs_to :category
-  attr_accessible :name, :price, :released_on, :category_id
+  attr_accessible :name, :price, :released_on, :category_id, :lock_version
 end
