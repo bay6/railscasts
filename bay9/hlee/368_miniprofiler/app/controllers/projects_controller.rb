19b6cd1c46b99fb6e8c22c785d768ce0d051b56b
Ken Lu <ken@seeice.com>
2013-12-02 15:10:05 -0500

    changed the query to make it query less

--- bay9/hlee/368_miniprofiler/app/controllers/projects_controller.rb
+++ bay9/hlee/368_miniprofiler/app/controllers/projects_controller.rb
@@ -1,7 +1,13 @@
 class ProjectsController < ApplicationController
+
+
   def index
-    @projects = Project.order(:created_at)
+    @projects = Project.order(:created_at).select("projects.*, count(tasks.id) as tasks_count").joins("left outer join tasks on project_id = projects.id").group("projects.id")
+    Rack::MiniProfiler.step("fetch projects") do
+      @projects.all
+    end
   end
+
 
   def show
     @project = Project.find(params[:id])
