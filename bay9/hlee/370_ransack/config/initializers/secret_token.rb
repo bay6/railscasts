ca35cbdb331a2af0aac2b17b7574f054d43e4a7b
Ken Lu <ken@seeice.com>
2013-12-02 08:21:57 -0500

    started learning 370 ransack for multiple search

---
+++ bay9/hlee/370_ransack/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Store::Application.config.secret_token = 'f9d732f9b5e9facffaf87c9f8092d429410fea929186845b9c9c817617fa6f5575a86c0b97c6504a94d67ac94eeca5971393a7f292c46f0ecfb531decdb3ed28'
