1f246825c8e31931266e8e21790087f099808e94
caok <caok1231@gmail.com>
2013-12-03 13:41:40 +0800

    finish 375

--- caok/375/config/deploy.rb
+++ caok/375/config/deploy.rb
@@ -7,6 +7,7 @@ load "config/recipes/postgresql"
 load "config/recipes/nodejs"
 load "config/recipes/rbenv"
 load "config/recipes/check"
+load "config/recipes/monit"
 
 server "69.164.192.207", :web, :app, :db, primary: true
 
