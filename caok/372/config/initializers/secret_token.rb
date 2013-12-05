f7f9446a527432ff90bf41d5ac4ebf6909055bc5
caok <caok1231@gmail.com>
2013-12-05 23:12:27 +0800

    init 372

---
+++ caok/372/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Store::Application.config.secret_token = 'fe400e5c10dd06acf3d2734468fda813592283bf9fc845d049b658b3e9f025c66d518dd187665361fe6cc16af623c94205622c89a894a9fe385364f75292986c'
