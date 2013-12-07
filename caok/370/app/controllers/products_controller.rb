29fae7d4c13982e36310460ad40676f3fc7a7d9d
caok <caok1231@gmail.com>
2013-12-07 13:13:41 +0800

    update

--- caok/370/app/controllers/products_controller.rb
+++ caok/370/app/controllers/products_controller.rb
@@ -2,7 +2,7 @@ class ProductsController < ApplicationController
   def index
     @search = Product.search(params[:q])
     @products = @search.result
-    @search.build_condition
+    @search.build_condition if @search.conditions.empty?
     @search.build_sort if @search.sorts.empty?
   end
 end
