695f4c5fdae91910d7ed94b7cb65b2c3b4746ead
Ken Lu <ken@seeice.com>
2013-12-06 22:00:46 -0800

    added two more methods to support the set released_text

--- bay9/hlee/016r_virtual_attributes/app/models/product.rb
+++ bay9/hlee/016r_virtual_attributes/app/models/product.rb
@@ -3,6 +3,8 @@ class Product < ActiveRecord::Base
   belongs_to :category
   has_many :taggings
   has_many :tags, through: :taggings
+  attr_writer :released_at_text
+  before_save :save_released_at_text
   validate :check_released_at_text
 
   def price_in_dollars
