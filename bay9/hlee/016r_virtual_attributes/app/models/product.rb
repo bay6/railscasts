9317bf43c2c6f2c1e9b7b39184989b671bd59346
Ken Lu <ken@seeice.com>
2013-12-06 21:35:41 -0800

    released at to virtual  attributes

--- bay9/hlee/016r_virtual_attributes/app/models/product.rb
+++ bay9/hlee/016r_virtual_attributes/app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ActiveRecord::Base
-  attr_accessible :name, :price_in_dollars, :released_at, :category_id
+  attr_accessible :name, :price_in_dollars, :released_at_text, :category_id
   belongs_to :category
   has_many :taggings
   has_many :tags, through: :taggings
@@ -10,5 +10,13 @@ class Product < ActiveRecord::Base
 
   def price_in_dollars=(dollars)
     self.price_in_cents = dollars.to_d * 100 if dollars.present?
+  end
+
+  def released_at_text
+    released_at.try(:strftime, "%Y-%m-%d %H:%M:%S")
+  end
+
+  def released_at_text=(time)
+    self.released_at = Time.zone.parse(time) if time.present?
   end
 end
