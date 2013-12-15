47905ceeed6d95458c6ac78ac5e771b378bde6e4
caok <caok1231@gmail.com>
2013-12-15 20:55:26 +0800

    finish 368

--- caok/368/app/controllers/application_controller.rb
+++ caok/368/app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :miniprofiler
+  private
+  def miniprofiler
+    <`2`>Rack::MiniProfiler.authorize_request if user.admin?
+  end
 end
