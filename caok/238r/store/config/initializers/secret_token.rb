1ab48ccd459d28a4a58abea31cb95f56af3d9e2a
caok <caok1231@gmail.com>
2013-12-17 11:55:00 +0800

    init 238rOC

---
+++ caok/238r/store/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Store::Application.config.secret_key_base = '906ffca352763617327c816086af3fa4fc38cd44758c79e0363a87117cea43ed5fede26f4405522a97a9e6a9280f1fdbb613b5001f450f52802fbc700cd309e4'
