4303814659b6bbacf0a34d4fd5513e0cb21214c6
caok <caok1231@gmail.com>
2013-12-17 22:24:11 +0800

    init 366

---
+++ caok/366/example/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Example::Application.config.secret_token = 'a5ba6e06b56873d72082bd5bff316b05e3df683592866a971e465a1c9746d28d023d2abfefe2a1aa9969e7e9b3dc096984da6c58cbced03130766b407447e43a'
