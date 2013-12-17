cf2bf486b899fb09d6a0462f75653e86e0a9361b
caok <caok1231@gmail.com>
2013-12-17 12:26:55 +0800

    add released_on in product

--- caok/238r/store/app/models/product.rb
+++ caok/238r/store/app/models/product.rb
@@ -2,4 +2,9 @@ class Product
   include Mongoid::Document
   field :name, type: String
   field :price, type: BigDecimal
+  field :released_on, type: Date
+
+  attr_accessible :name, :price, :released_on
+
+  validates_presence_of :name
 end
