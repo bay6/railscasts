32fb52b03446465bac621113850766fa97399bcf
Ken Lu <ken@seeice.com>
2013-12-18 09:38:11 -0500

    works without duplicated times submit

--- bay9/hlee/364_active_record_reputation/app/controllers/haikus_controller.rb
+++ bay9/hlee/364_active_record_reputation/app/controllers/haikus_controller.rb
@@ -13,4 +13,11 @@ class HaikusController < ApplicationController
     @haiku = current_user.haikus.create!(params[:haiku])
     redirect_to @haiku, notice: "Successfully created haiku."
   end
+
+  def vote
+    value = params[:type] == "up" ? 1 : -1
+    @haiku = Haiku.find(params[:id])
+    @haiku.add_evaluation(:votes, value, current_user)
+    redirect_to :back, notice: "Thank you for voting!"
+  end
 end
