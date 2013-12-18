780e0d84aff0d5ea99e1daa5e41c9fb0d63e0fea
xiongbo <xiongbo027@gmail.com>
2013-12-18 20:20:27 +0800

    add virtual attributes for dollars

--- xiongbo/16r-virtual-attributes/store/app/models/product.rb
+++ xiongbo/16r-virtual-attributes/store/app/models/product.rb
@@ -1,6 +1,14 @@
 class Product < ActiveRecord::Base
-  attr_accessible :name, :price_in_cents, :released_at, :category_id
+  attr_accessible :name, :price_in_dollars, :released_at, :category_id
   belongs_to :category
   has_many :taggings
   has_many :tags, through: :taggings
+
+  def price_in_dollars
+    price_in_cents.to_d / 100 if price_in_cents
+  end
+
+  def price_in_dollars=(dollars)
+    self.price_in_cents = dollars.to_d * 100 if dollars.present?
+  end
 end
