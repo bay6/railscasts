9cf3d764df6846d82f9d9910b3104cc86ac62bc8
xiongbo <xiongbo027@gmail.com>
2013-12-18 21:44:11 +0800

    refactor released_at_text attribute

--- xiongbo/16r-virtual-attributes/store/app/models/product.rb
+++ xiongbo/16r-virtual-attributes/store/app/models/product.rb
@@ -4,8 +4,10 @@ class Product < ActiveRecord::Base
   has_many :taggings
   has_many :tags, through: :taggings
 
+  attr_writer :released_at_text
+  before_save :save_released_at_text
 
-  validates_presence_of :released_at
+  validates_presence_of :check_released_at_text
 
   def price_in_dollars
     price_in_cents.to_d / 100 if price_in_cents
@@ -16,12 +18,19 @@ class Product < ActiveRecord::Base
   end
 
   def released_at_text
-    released_at.try(:strftime, "%Y-%m-%d %H:%M:%S")
+    @released_at_text || released_at.try(:strftime, "%Y-%m-%d %H:%M:%S")
   end
 
-  def released_at_text=(time)
-    self.released_at = Time.zone.parse(time) if time.present?
-  rescue ArgumentError
-    self.released_at = nil
+  def save_released_at_text
+    self.released_at = Time.zone.parse(@released_at_text) if @released_at_text.present?
   end
+
+  def check_released_at_text
+    if @released_at_text.present? && Time.zone.parse(@released_at_text).nil?
+      errors.add :released_at_text, "cant be parsed"
+    end
+  rescue ArgumentError
+    errors.add :released_at_text, "is out of range"
+  end
+
 end
