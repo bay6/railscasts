50a3cf7b5fd225b4ee85c98b6c124d86b424e853
caok <caok1231@gmail.com>
2013-12-18 22:40:46 +0800

    add  feed_counter

---
+++ caok/367/feed_counter.rb
@@ -0,0 +1,21 @@
+require 'rss'
+require 'open-uri'
+
+class FeedCounter
+  def initialize(url)
+    @url = url
+  end
+
+  def count
+    open(@url) do |f|
+      rss = RSS::Parser.parse(f.read, false)
+      count = rss.items.size
+      puts "#{count} in #{@url}"
+      count
+    end
+  end
+end
+
+counts = $*.map { |url| FeedCounter.new(url).count }
+total = counts.inject(:+)
+puts "#{total} total" if total
--- caok/367/rocket.rb
+++ caok/367/rocket.rb
@@ -3,12 +3,29 @@ require 'celluloid'
 class Rocket
   include Celluloid
 
+  def initialize(autolaunch = false)
+    launch! if autolaunch
+  end
+
   def launch
-    raise "Houston, we have a problem"
     3.downto(1) do |n|
       puts "#{n}..."
       sleep 1
+      raise "Houston, we have a problem" if [true, false].sample
     end
     puts "Blast off!"
+  end
+end
+
+class Launcher
+  include Celluloid
+  trap_exit :relaunch
+
+  def launch_rocket
+    Rocket.new_link.launch!
+  end
+
+  def relaunch(actor, reason)
+    launch_rocket
   end
 end
