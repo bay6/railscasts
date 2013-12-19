b061027aff281a7fa5a9a2efc246ea66bef2e798
Ken Lu <ken@seeice.com>
2013-12-19 09:27:57 -0500

    updated gem and setting for user

--- bay9/hlee/235r_devise_and_omniauth/app/models/user.rb
+++ bay9/hlee/235r_devise_and_omniauth/app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :omniauthable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :username
+attr_accessible :email, :password, :password_confirmation, :remember_me, :username
   
   validates_presence_of :username
   validates_uniqueness_of :username
