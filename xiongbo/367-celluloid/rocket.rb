971e160f7afa27bafca4e97d4ee5139343c98493
xiongbo <xiongbo027@gmail.com>
2013-12-20 21:30:32 +0800

    add new launcher for test new_link

--- xiongbo/367-celluloid/rocket.rb
+++ xiongbo/367-celluloid/rocket.rb
@@ -4,11 +4,21 @@ class Rocket
   include Celluloid
 
   def lanuch
+    raise "Wuhan, we have a problem"
     3.downto(1) do |n|
       puts "#{n}......"
       sleep 1
     end
 
     puts "blast off!!!!"
+  end
+end
+
+
+class Launcher
+  include Celluloid
+
+  def launch_rocket
+    Rocket.new_link.async.lanuch
   end
 end
