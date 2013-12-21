3435f4582282d5b2ae7fd05645ddeba02edccfcb
Ken Lu <ken@seeice.com>
2013-12-21 11:04:16 -0500

    change the credential

--- bay9/hlee/235r_devise_and_omniauth/app/controllers/omniauth_callbacks_controller.rb
+++ bay9/hlee/235r_devise_and_omniauth/app/controllers/omniauth_callbacks_controller.rb
@@ -1,2 +1,7 @@
-class OmniauthCallbacksController < ApplicationController
+class OmniauthCallbacksController < Devise::OmniauthCallbacksController 
+  def all
+    #raise request.env["omniauth.auth"].to_yaml
+    user = User.from_omniauth(request.env["omniauth.auth"])
+  end
+  alias_method :twitter, :all
 end
--- bay9/hlee/235r_devise_and_omniauth/config/initializers/devise.rb
+++ bay9/hlee/235r_devise_and_omniauth/config/initializers/devise.rb
@@ -230,5 +230,5 @@ Devise.setup do |config|
   # When using omniauth, Devise cannot automatically set Omniauth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = "/my_engine/users/auth"
-  config.omniauth :twitter, '1863636288-Ecsxgw5zncRVTuLoPRLDdx5ApPcuSGrcoBxNnHP', 'maQDDGGyZ0xaXlOx2liLmzAag2KqUWqwrdQq3imsDuQ' 
+  config.omniauth :twitter, 'HdvvOP7MKzMsQErS6CurXA', 'VPRYvu82UA26WWL5t6jo2KyKjw2TZ1SpJENzeiE' 
 end
