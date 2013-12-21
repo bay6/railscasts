877a29034a74c751a2675166f24e68d509060a82
Ken Lu <ken@seeice.com>
2013-12-21 11:36:55 -0500

    finished 235 devise and omniauth revised

--- bay9/hlee/235r_devise_and_omniauth/app/controllers/omniauth_callbacks_controller.rb
+++ bay9/hlee/235r_devise_and_omniauth/app/controllers/omniauth_callbacks_controller.rb
@@ -1,7 +1,13 @@
-class OmniauthCallbacksController < Devise::OmniauthCallbacksController 
+class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def all
-    #raise request.env["omniauth.auth"].to_yaml
     user = User.from_omniauth(request.env["omniauth.auth"])
+    if user.persisted?
+      flash.notice = "Signed in!"
+      sign_in_and_redirect user
+    else
+      session["devise.user_attributes"] = user.attributes
+      redirect_to new_user_registration_url
+    end
   end
   alias_method :twitter, :all
 end
