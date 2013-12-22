57a35d0c0014c512a74caf545a3293a4e3f40c30
Ken Lu <ken@seeice.com>
2013-12-22 17:05:11 -0500

    export to csv

--- bay9/hlee/362_exporting_csv_and_execel/app/controllers/products_controller.rb
+++ bay9/hlee/362_exporting_csv_and_execel/app/controllers/products_controller.rb
@@ -1,5 +1,9 @@
 class ProductsController < ApplicationController
   def index
     @products = Product.order(:name)
+    respond_to do |format|
+      format.html
+      format.csv { render text: @products.to_csv }
+    end
   end
 end
--- bay9/hlee/362_exporting_csv_and_execel/app/models/product.rb
+++ bay9/hlee/362_exporting_csv_and_execel/app/models/product.rb
@@ -1,3 +1,12 @@
 class Product < ActiveRecord::Base
   attr_accessible :name, :price, :released_on
+
+  def self.to_csv
+    CSV.generate do |csv|
+      csv << column_names
+      all.each do |product|
+        csv << product.attributes.values_at(*column_names)
+      end
+    end
+  end
 end
