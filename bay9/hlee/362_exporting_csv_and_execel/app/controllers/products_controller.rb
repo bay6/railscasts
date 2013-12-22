60ebe9b73b7ec948945ec0513cbdca6b6162e8c4
Ken Lu <ken@seeice.com>
2013-12-22 17:11:52 -0500

    Finished 362 export and csv and excel

--- bay9/hlee/362_exporting_csv_and_execel/app/controllers/products_controller.rb
+++ bay9/hlee/362_exporting_csv_and_execel/app/controllers/products_controller.rb
@@ -3,7 +3,8 @@ class ProductsController < ApplicationController
     @products = Product.order(:name)
     respond_to do |format|
       format.html
-      format.csv { render text: @products.to_csv }
+      format.csv { send_data @products.to_csv }
+      format.xls
     end
   end
 end
--- bay9/hlee/362_exporting_csv_and_execel/app/models/product.rb
+++ bay9/hlee/362_exporting_csv_and_execel/app/models/product.rb
@@ -1,8 +1,8 @@
 class Product < ActiveRecord::Base
   attr_accessible :name, :price, :released_on
-
-  def self.to_csv
-    CSV.generate do |csv|
+  
+  def self.to_csv(options = {})
+    CSV.generate(options) do |csv|
       csv << column_names
       all.each do |product|
         csv << product.attributes.values_at(*column_names)
