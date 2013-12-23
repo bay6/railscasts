944fbaffee158eccd845bed0feedc617cda453b4
Ken Lu <ken@seeice.com>
2013-12-23 08:48:28 -0500

    removed the sandbox problem

--- bay9/hlee/363_facebook_open_graph/config/initializers/omniauth.rb
+++ bay9/hlee/363_facebook_open_graph/config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET'], scope: "email,publish_stream"
+  provider :facebook, '425072827621438', 'c1943c93005c4fad7753247398605a72', scope: "email,publish_stream"
 end
