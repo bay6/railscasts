3e859820f0362aeacb856a4a4a41feecd88d9f1a
Ken Lu <ken@seeice.com>
2013-12-24 21:46:33 -0500

    reviews

--- bay9/hlee/363_facebook_open_graph/app/controllers/reviews_controller.rb
+++ bay9/hlee/363_facebook_open_graph/app/controllers/reviews_controller.rb
@@ -2,6 +2,9 @@ class ReviewsController < ApplicationController
   def create
     @movie = Movie.find(params[:movie_id])
     @review = @movie.reviews.create!(params[:review])
+    if current_user
+      current_user.facebook.put_connections("me", "cinematron:review", movie: movie_url(@movie))
+    end
     redirect_to @review.movie, notice: "Review has been created."
   end
 end
