70cde6e40e838567c08cd6c4cbb71d86e9b55334
Ken Lu <ken@seeice.com>
2013-12-24 22:25:49 -0500

    routs and authenticate

--- bay9/hlee/360_facebook_authentication/config/routes.rb
+++ bay9/hlee/360_facebook_authentication/config/routes.rb
@@ -1,4 +1,8 @@
 Cinema::Application.routes.draw do
+  match 'auth/:provider/callback', to: 'sessions#create'
+  match 'auth/failure', to: redirect('/')
+  match 'signout', to: 'sessions#destroy', as: 'signout'
+  
   resources :movies do
     resources :reviews
   end
