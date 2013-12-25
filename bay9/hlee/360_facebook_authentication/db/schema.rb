da22c8e4a82ac9f7aa51541d2f2e217e70e3760b
Ken Lu <ken@seeice.com>
2013-12-24 22:26:29 -0500

    added user table

--- bay9/hlee/360_facebook_authentication/db/schema.rb
+++ bay9/hlee/360_facebook_authentication/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120620204132) do
+ActiveRecord::Schema.define(:version => 20131225032603) do
 
   create_table "movies", :force => true do |t|
     t.string   "name"
@@ -31,5 +31,15 @@ ActiveRecord::Schema.define(:version => 20120620204132) do
   end
 
   add_index "reviews", ["movie_id"], :name => "index_reviews_on_movie_id"
+
+  create_table "users", :force => true do |t|
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "name"
+    t.string   "oauth_token"
+    t.datetime "oauth_expires_at"
+    t.datetime "created_at",       :null => false
+    t.datetime "updated_at",       :null => false
+  end
 
 end
