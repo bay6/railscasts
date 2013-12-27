bb9aa33acf75eef10a88cd6e9dde8dab7d10e779
caok <caok1231@gmail.com>
2013-12-27 22:40:58 +0800

    finish 365

---
+++ caok/365/thready/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thready::Application.config.secret_token = 'ec08291188a33f470e029175df7b142de80e8b36673d248b1b6cf8090c6c1682103d9faa83b1d53c73bca85a99339a370ef306da1cba0accd8f245bf4662c200'
