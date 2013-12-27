69b8e39a710a2638014e37af1528662a7217030a
Ken Lu <ken@seeice.com>
2013-12-27 21:51:34 +0000

    finished 361 facebook graph api

--- bay9/hlee/361_facebook_graph_api/app/models/user.rb
+++ bay9/hlee/361_facebook_graph_api/app/models/user.rb
@@ -12,6 +12,14 @@ class User < ActiveRecord::Base
 
   def facebook
     @facebook ||= Koala::Facebook::API.new(oauth_token)
+    block_given? ? yield(@facebook) : @facebook
+  rescue Koala::Facebook::APIError
+    logger.info e.to_s
+    nil
+  end
+
+  def friends_count
+    facebook { |fb| fb.get_connection("me", "friends").size }
   end
 
 end
