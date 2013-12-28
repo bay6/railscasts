f9570574ae21bbd8b4b48a13dc3d6200183272e5
xiongbo <xiongbo027@gmail.com>
2013-12-28 21:25:06 +0800

    play with puma

--- xiongbo/365-thread-safety/thready/app/controllers/foo_controller.rb
+++ xiongbo/365-thread-safety/thready/app/controllers/foo_controller.rb
@@ -1,6 +1,10 @@
 class FooController < ApplicationController
+  @@counter = 0
   def bar
+    counter = @@counter
     sleep 1
+    counter += 1
+    @@counter = counter
     render text: "foobar\n"
   end
 end
