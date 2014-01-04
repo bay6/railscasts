f4a5e82802af8aeffd2b6a88ebd859827f9be0c1
xiongbo <xiongbo027@gmail.com>
2014-01-04 18:02:13 +0800

    update vote for user

--- xiongbo/364-active-record-reputation-system/youhaiku/app/models/user.rb
+++ xiongbo/364-active-record-reputation-system/youhaiku/app/models/user.rb
@@ -4,6 +4,11 @@ class User < ActiveRecord::Base
   validates_uniqueness_of :name
 
   has_many :haikus
+  has_many :evalutations, class_name: "RSEvalutation", as: :source
 
   has_reputation :votes, source: { reputation: :votes, of: :haikus }, aggregated_by: :sum
+
+  def voted_for?(haiku)
+    evaluations.where(target_type: haiku.class, target_id: haiku.id).present?
+  end
 end
