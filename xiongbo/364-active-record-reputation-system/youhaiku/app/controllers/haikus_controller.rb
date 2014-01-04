09533e3c703947a909cf348b5ecfd8e10c5114e5
xiongbo <xiongbo027@gmail.com>
2014-01-04 17:51:29 +0800

    update votes

--- xiongbo/364-active-record-reputation-system/youhaiku/app/controllers/haikus_controller.rb
+++ xiongbo/364-active-record-reputation-system/youhaiku/app/controllers/haikus_controller.rb
@@ -2,7 +2,7 @@ class HaikusController < ApplicationController
   before_filter :authorize, except: [:index, :show]
 
   def index
-    @haikus = Haiku.all
+    @haikus = Haiku.find_with_reputation(:votes, :all, order: 'votes desc')
   end
 
   def show
@@ -17,7 +17,7 @@ class HaikusController < ApplicationController
   def vote
     value = params[:type] == "up" ? 1 : -1
     @haiku = Haiku.find(params[:id])
-    @haiku.add_evaluation(:votes, value, current_user)
+    @haiku.add_or_update_evaluation(:votes, value, current_user)
     redirect_to :back, notice: "Thank you for voting"
   end
 end
