5d4c9a3a7d9cb594951d98c1abf473bd25dc12d5
Ken Lu <ken@seeice.com>
2014-01-06 08:59:03 -0500

    resolved the ssl warning

--- bay9/hlee/357_adding_ssl/todo-before/app/models/task.rb
+++
@@ -1,4 +0,0 @@
-class Task < ActiveRecord::Base
-  belongs_to :user
-  attr_accessible :name
-end
--- bay9/hlee/357_adding_ssl/todo-before/app/models/user.rb
+++
@@ -1,5 +0,0 @@
-class User < ActiveRecord::Base
-  attr_accessible :name, :password, :password_confirmation
-  has_secure_password
-  has_many :tasks
-end
