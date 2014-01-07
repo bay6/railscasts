76693e8c077a9ef158d88331e6105c0919778b94
caok <caok1231@gmail.com>
2014-01-07 23:25:27 +0800

    finish 209r

--- caok/209r/app/controllers/articles_controller.rb
+++ caok/209r/app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_filter :authenticate_user!, except: [:index, :show]
   def index
     @articles = Article.all
   end
--- caok/209r/config/routes.rb
+++ caok/209r/config/routes.rb
@@ -1,5 +1,5 @@
 Blog::Application.routes.draw do
-  devise_for :users
+  devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}
 
   resources :articles
   root to: 'articles#index'
