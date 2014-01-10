1cbac5c4afb28cd8a2ba092b0da049392528373d
Ken <ken@seeice.com>
2014-01-10 18:59:48 -0500

    session controll and user storage

--- bay9/hlee/241r_simple_omniauth/app/controllers/sessions_controller.rb
+++ bay9/hlee/241r_simple_omniauth/app/controllers/sessions_controller.rb
@@ -1,6 +1,7 @@
 class SessionsController < ApplicationController
   def create
-   binding.pry
-   raise env['omniauth.auth'].to_yaml
+   user = User.from_omniauth(env['omniauth.auth'])
+   session[:user_id] = user.id
+   redirect_to root_url, notice: "Signed in."
   end
 end
