98d3a1e82ccdb8bd00b96856c579acb7b308d057
Ken <ken@seeice.com>
2014-01-10 18:57:33 -0500

    updated the database

--- bay9/hlee/241r_simple_omniauth/db/schema.rb
+++ bay9/hlee/241r_simple_omniauth/db/schema.rb
@@ -11,12 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140109233919) do
+ActiveRecord::Schema.define(version: 20140110235642) do
 
   create_table "articles", force: true do |t|
     t.string   "name"
     t.text     "content"
     t.datetime "published_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
