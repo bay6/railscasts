b89ff8ba7d4e5a313d1b355b4fc4f358a9cf343b
xiongbo <xiongbo027@gmail.com>
2014-01-11 13:17:28 +0800

    fixed with user update

--- xiongbo/235r-devise-and-omniauth/blog/app/models/user.rb
+++ xiongbo/235r-devise-and-omniauth/blog/app/models/user.rb
@@ -30,4 +30,12 @@ class User < ActiveRecord::Base
   def password_required?
     super && provider.blank?
   end
+
+  def update_with_password(params, *options)
+    if encrypted_password.blank?
+      update_attributes(params, *options)
+    else
+      super
+    end
+  end
 end
