4518bdbec24745422c15ac768412ef3cf2e9357b
xiongbo <xiongbo027@gmail.com>
2014-01-11 13:09:22 +0800

    correct password required error

--- xiongbo/235r-devise-and-omniauth/blog/app/controllers/omniauth_callbacks_controller.rb
+++ xiongbo/235r-devise-and-omniauth/blog/app/controllers/omniauth_callbacks_controller.rb
@@ -4,6 +4,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     if user.persisted?
       sign_in_and_redirect user, notice: "signed in"
     else
+      session["devise.user_attributes"] = user.attributes
       redirect_to new_user_registration_url
     end
   end
--- xiongbo/235r-devise-and-omniauth/blog/app/models/user.rb
+++ xiongbo/235r-devise-and-omniauth/blog/app/models/user.rb
@@ -15,4 +15,19 @@ class User < ActiveRecord::Base
       user.username = auth.info.nickname
     end
   end
+
+  def self.new_with_session(params, session)
+    if session["devise.user_attributes"]
+      new(session["devise.user_attributes"], without_protection: true) do |user|
+        user.attributes = params
+        user.valid?
+      end
+    else
+      super
+    end
+  end
+
+  def password_required?
+    super && provider.blank?
+  end
 end
