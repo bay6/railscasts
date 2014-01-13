8be739021e180d8d3819c46d39d29c0c12406333
Ken <ken@seeice.com>
2014-01-13 17:56:52 -0500

    started to improve the search where clause

--- bay9/hlee/355_hacking_with_arel/app/models/product.rb
+++ bay9/hlee/355_hacking_with_arel/app/models/product.rb
@@ -5,4 +5,8 @@ class Product < ActiveRecord::Base
   def self.search(query)
     where("released_at <= ? and (discontinued_at is null or discontinued_at > ?) and stock >= ? and name like ?", Time.zone.now, Time.zone.now, 2, "%#{query}%")
   end
+
+  def self.search1(query)
+    where("released_at <= :now and (discontinued_at is null or discontinued_at > :now) and stock >= :stock and name like :query", now: Time.zone.now, stock: 2, query: "%#{query}%")
+  end
 end
