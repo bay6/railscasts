d50e5fa0334d6314b051206ddbc879db60b77a51
Ken Lu <ken@seeice.com>
2014-01-12 20:24:36 -0500

    added function of signout

--- bay9/hlee/241r_simple_omniauth/config/routes.rb
+++ bay9/hlee/241r_simple_omniauth/config/routes.rb
@@ -6,6 +6,8 @@ SimpleOmniauth::Application.routes.draw do
   #root to: 'articles#index'
   get 'auth/:provider/callback', to: 'sessions#create'
 
+  get 'signout', to: 'sessions#destroy', as: 'signout'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
