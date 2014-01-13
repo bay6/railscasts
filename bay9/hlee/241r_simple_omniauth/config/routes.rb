8f3cc089f890b042e31cf534d6a15f1072ea4d41
Ken Lu <ken@seeice.com>
2014-01-12 20:18:28 -0500

    updated routes to rails 4

--- bay9/hlee/241r_simple_omniauth/config/routes.rb
+++ bay9/hlee/241r_simple_omniauth/config/routes.rb
@@ -1,8 +1,8 @@
 Blog::Application.routes.draw do
   root :to => "articles#index"
   
-  match "/auth/:provider/callback" => "sessions#create"
-  match "/signout" => "sessions#destroy", :as => :signout
+  get "/auth/:provider/callback" => "sessions#create"
+  get "/signout" => "sessions#destroy", :as => :signout
   
   resources :comments
   resources :articles
