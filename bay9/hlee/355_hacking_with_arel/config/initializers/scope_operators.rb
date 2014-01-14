1a90f737837c8000a9e5fa64e39cbb07d0a45804
Ken <ken@seeice.com>
2014-01-14 18:36:12 -0500

    enhanced version of scope operators

--- bay9/hlee/355_hacking_with_arel/config/initializers/scope_operators.rb
+++ bay9/hlee/355_hacking_with_arel/config/initializers/scope_operators.rb
@@ -7,6 +7,20 @@ module ScopeOperators
     scope
   end
   alias_method :|, :or
+
+  def not(other)
+    left = arel.constraints.reduce(:and)
+    right = other.arel.constraints.reduce(:and)
+    scope = merge(other)
+    scope.where_values = [left, right.not]
+    scope
+  end
+  alias_method :-, :not
+
+  def and(*args)
+    merge(*args)
+  end
+  alias_method :&, :and
 end
 
 ActiveSupport.on_load :active_record do
