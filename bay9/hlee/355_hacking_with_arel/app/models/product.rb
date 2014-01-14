9f6da780e4bb476aa00abbb86e7e57ad55806710
Ken <ken@seeice.com>
2014-01-14 18:31:56 -0500

    scope generator hack way

--- bay9/hlee/355_hacking_with_arel/app/models/product.rb
+++ bay9/hlee/355_hacking_with_arel/app/models/product.rb
@@ -3,10 +3,12 @@ class Product < ActiveRecord::Base
   attr_accessible :name, :price, :category_id, :released_at, :discontinued_at, :stock
 
   scope :released,          -> { where("released_at <= ?", Time.zone.now) }
+  scope :released,          -> { released_at_lt(Time.zone.now) }
   scope :not_discontinued,  -> { where("discontinued_at is null or discontinued_at > ?", Time.zone.now) }
   scope :in_stock,          -> { where("stock >= ?", 2) }
   scope :available,         -> { released.not_discontinued.in_stock }
   scope :search,            -> { available.where("name like ?", "%#{query}%") }
+
 
   def self.search(query)
     where("released_at <= ? and (discontinued_at is null or discontinued_at > ?) and stock >= ? and name like ?", Time.zone.now, Time.zone.now, 2, "%#{query}%")
