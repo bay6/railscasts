3e099774a04c908b41d998447864888d3a83111b
Ken Lu <ken@seeice.com>
2014-01-20 18:52:21 -0500

    search function with squeel

--- bay9/hlee/354_squeel/app/models/product.rb
+++ bay9/hlee/354_squeel/app/models/product.rb
@@ -2,7 +2,15 @@ class Product < ActiveRecord::Base
   belongs_to :category
   attr_accessible :name, :price, :category_id, :released_at, :discontinued_at, :stock
 
+  #def self.search(query)
+    #where("released_at <= ? and (discontinued_at is null or discontinued_at > ?) and stock >= ? and name like ?", Time.zone.now, Time.zone.now, 2, "%#{query}%")
+  #end
+  #
   def self.search(query)
-    where("released_at <= ? and (discontinued_at is null or discontinued_at > ?) and stock >= ? and name like ?", Time.zone.now, Time.zone.now, 2, "%#{query}%")
+    where do 
+      (released_at <= Time.zone.now) & 
+    ((discontinued_at == nil) | (discontinued_at < Time.zone.now)) & 
+    (stock >= 2) & (name =~ "%#{query}%")
+    end
   end
 end
