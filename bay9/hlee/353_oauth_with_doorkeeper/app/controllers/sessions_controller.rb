c5ce2e745a80cc532815d35fc2ab9b03b454d9f4
Ken Lu <ken@seeice.com>
2014-01-26 12:42:03 -0500

    fixed return problem and the administration part

--- bay9/hlee/353_oauth_with_doorkeeper/app/controllers/sessions_controller.rb
+++ bay9/hlee/353_oauth_with_doorkeeper/app/controllers/sessions_controller.rb
@@ -7,7 +7,12 @@ class SessionsController < ApplicationController
     user = User.find_by_name(params[:name])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to(session.delete(:return_to) || root_url)
+      if session[:return_to]
+        redirect_to session[:return_to]
+        session[:return_to] = nil
+      else
+        redirect_to root_url
+      end
     else
       flash.now.alert = "Email or password is invalid"
       render "new"
--- bay9/hlee/353_oauth_with_doorkeeper/config/initializers/doorkeeper.rb
+++ bay9/hlee/353_oauth_with_doorkeeper/config/initializers/doorkeeper.rb
@@ -17,6 +17,13 @@ Doorkeeper.configure do
   #   # Example implementation:
   #   Admin.find_by_id(session[:admin_id]) || redirect_to(new_admin_session_url)
   # end
+  admin_authenticator do |routes|
+    # Put your admin authentication logic here.
+    # If you want to use named routes from your app you need
+    # to call them on routes object eg.
+    # routes.new_admin_session_path
+    session[:user_id] == 1 || redirect_to(routes.login_url(return_to: request.fullpath))
+  end
 
   # Authorization Code expiration time (default 10 minutes).
   # authorization_code_expires_in 10.minutes
