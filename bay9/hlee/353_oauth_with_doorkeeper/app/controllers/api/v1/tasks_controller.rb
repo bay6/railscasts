4270528db7e44d52766521b4fd7fb58388580e3d
Ken Lu <ken@seeice.com>
2014-01-25 19:31:49 -0500

    finished 353 auth with door keeper

--- bay9/hlee/353_oauth_with_doorkeeper/app/controllers/api/v1/tasks_controller.rb
+++ bay9/hlee/353_oauth_with_doorkeeper/app/controllers/api/v1/tasks_controller.rb
@@ -1,15 +1,21 @@
 module Api
   module V1
     class TasksController < ApplicationController
+      doorkeeper_for :all
       respond_to :json
-
       def index
-        respond_with Task.all
+        respond_with current_user.tasks
       end
 
       def create
-        respond_with Task.create(params[:task])
+        respond_with current_user.tasks.create(params[:task])
       end
+
+      private
+      def current_user
+        @current_user ||= User.find(doorkeeper_token.resource_owner_id)
+      end
+
     end
   end
 end
