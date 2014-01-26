64a04338db08e575cb78ee50be8d58f09d02d394
Ken Lu <ken@seeice.com>
2014-01-26 12:39:26 -0500

    updated the api with more info

--- bay9/hlee/353_oauth_with_doorkeeper/app/controllers/api/v1/tasks_controller.rb
+++ bay9/hlee/353_oauth_with_doorkeeper/app/controllers/api/v1/tasks_controller.rb
@@ -1,8 +1,9 @@
 module Api
   module V1
-    class TasksController < ApplicationController
+    class TasksController < BaseController
       doorkeeper_for :all
       respond_to :json
+
       def index
         respond_with current_user.tasks
       end
@@ -10,12 +11,6 @@ module Api
       def create
         respond_with current_user.tasks.create(params[:task])
       end
-
-      private
-      def current_user
-        @current_user ||= User.find(doorkeeper_token.resource_owner_id)
-      end
-
     end
   end
 end
