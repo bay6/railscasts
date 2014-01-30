6b726277dda5fccd57b2e1188927764d7df09c1e
Ken Lu <ken@seeice.com>
2014-01-29 21:19:09 -0500

    changed schema

--- bay9/hlee/352_securing_an_api/db/schema.rb
+++ bay9/hlee/352_securing_an_api/db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120515210216) do
+ActiveRecord::Schema.define(:version => 20140130021821) do
+
+  create_table "api_keys", :force => true do |t|
+    t.string   "access_token"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
+  end
 
   create_table "categories", :force => true do |t|
     t.string   "name"
