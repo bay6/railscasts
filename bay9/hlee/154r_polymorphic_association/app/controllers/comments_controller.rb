dc4931389d1c2c63fde45fc6586490a0b193fba1
Ken Lu <ken@seeice.com>
2014-01-30 09:21:27 -0500

    load commentable at controller

--- bay9/hlee/154r_polymorphic_association/app/controllers/comments_controller.rb
+++ bay9/hlee/154r_polymorphic_association/app/controllers/comments_controller.rb
@@ -1,9 +1,17 @@
 class CommentsController < ApplicationController
+  before_filter :load_commentable
+
   def index
-    @commentable = Article.find(params[:article_id])
     @comments = @commentable.comments
   end
 
   def new
   end
+
+private
+
+  def load_commentable
+    resource, id = request.path.split('/')[1,2]
+    @commentable = resource.singularize.classify.constantize.find(id)
+  end  
 end
