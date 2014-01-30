79ac59ced1257c72715f64f1007d4210531c7a49
Ken Lu <ken@seeice.com>
2014-01-29 21:24:06 -0500

    another authenticate with http token

--- bay9/hlee/352_securing_an_api/app/controllers/api/v1/products_controller.rb
+++ bay9/hlee/352_securing_an_api/app/controllers/api/v1/products_controller.rb
@@ -28,8 +28,9 @@ module Api
 
       private
       def restrict_access
-        api_key = ApiKey.find_by_access_token(params[:access_token])
-        head :unauthorized unless api_key
+        authenticate_or_request_with_http_token do |token, options|
+          ApiKey.exists?(access_token: token)
+        end
       end
     end
   end
