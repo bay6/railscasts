fa236edf7db06e3e330e0cdbe3b2267d6fcfae73
Ken Lu <ken@seeice.com>
2014-01-29 21:36:31 -0500

    updated the migration

--- bay9/hlee/154r_polymorphic_association/db/migrate/20140130023141_create_comments.rb
+++ bay9/hlee/154r_polymorphic_association/db/migrate/20140130023141_create_comments.rb
@@ -2,10 +2,11 @@ class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
       t.text :content
-      t.integer :commentable_id
-      t.string :commentable_type
+      t.belongs_to :commentable, polymorphic: true
 
       t.timestamps
     end
+    add_index :comments, [:commentable_id, :commentable_type]
+
   end
 end
