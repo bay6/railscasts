bcc36f5099b422cd0d1c5ae0c0dd5f743ba7059c
Ken Lu <ken@seeice.com>
2014-01-29 21:20:14 -0500

    generate access token by random

--- bay9/hlee/352_securing_an_api/app/models/api_key.rb
+++ bay9/hlee/352_securing_an_api/app/models/api_key.rb
@@ -1,3 +1,12 @@
 class ApiKey < ActiveRecord::Base
-  attr_accessible :access_token
+  before_create :generate_access_token
+  
+private
+  
+  def generate_access_token
+    begin
+      self.access_token = SecureRandom.hex
+    end while self.class.exists?(access_token: access_token)
+  end
+
 end
