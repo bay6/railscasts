4a8237a7b4f6751539ddb2fec6ce7bc06eaa28f9
Ken Lu <ken@seeice.com>
2014-01-29 21:22:58 -0500

    before action for controller to add api key to head

--- bay9/hlee/352_securing_an_api/app/controllers/api/v1/products_controller.rb
+++ bay9/hlee/352_securing_an_api/app/controllers/api/v1/products_controller.rb
@@ -2,6 +2,8 @@ module Api
   module V1
     class ProductsController < ApplicationController
       http_basic_authenticate_with name: 'admin', password: 'secret'
+      before_filter :restrict_access
+
       respond_to :json
 
       def index
@@ -22,6 +24,12 @@ module Api
 
       def destroy
         respond_with Product.destroy(params[:id])
+      end
+
+      private
+      def restrict_access
+        api_key = ApiKey.find_by_access_token(params[:access_token])
+        head :unauthorized unless api_key
       end
     end
   end
