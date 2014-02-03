cd70d0420efe0e4233802f7bfcfd7f04e31b72b3
Ken Lu <ken@seeice.com>
2014-02-03 09:16:11 -0500

    routes for both versions of api

--- bay9/hlee/350_rest_api_versioning/config/routes.rb
+++ bay9/hlee/350_rest_api_versioning/config/routes.rb
@@ -3,8 +3,10 @@ Store::Application.routes.draw do
     namespace :v1 do
       resources :products
     end
+    namespace :v2 do
+      resources :products
+    end
   end
-  
   resources :products
   root to: 'products#index'
 end
