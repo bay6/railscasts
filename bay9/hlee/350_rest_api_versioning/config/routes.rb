56d66cfbe5ebb999bde21c76365f6b585e718b0d
Ken Lu <ken@seeice.com>
2014-02-03 09:13:32 -0500

    added one more field and the migration

--- bay9/hlee/350_rest_api_versioning/config/routes.rb
+++ bay9/hlee/350_rest_api_versioning/config/routes.rb
@@ -1,9 +1,10 @@
 Store::Application.routes.draw do
-  namespace :api do
+  namespace :api, defaults: {format: 'json'} do
     namespace :v1 do
       resources :products
     end
   end
+  
   resources :products
   root to: 'products#index'
 end
