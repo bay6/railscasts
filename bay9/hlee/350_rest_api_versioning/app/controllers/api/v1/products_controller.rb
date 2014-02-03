d1a70193f6ac5e734b1979e9dd6a8d25682f805d
Ken Lu <ken@seeice.com>
2014-02-03 09:11:22 -0500

    added one more route and controller for the api

---
+++ bay9/hlee/350_rest_api_versioning/app/controllers/api/v1/products_controller.rb
@@ -0,0 +1,7 @@
+module Api
+  module V1
+    class ProductsController < ApplicationController
+      
+    end
+  end
+end
--- bay9/hlee/350_rest_api_versioning/config/routes.rb
+++ bay9/hlee/350_rest_api_versioning/config/routes.rb
@@ -1,4 +1,9 @@
 Store::Application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :products
+    end
+  end
   resources :products
   root to: 'products#index'
 end
