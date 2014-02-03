80905a1540f0524b4ebc09d5b38894510dd24c7b
Ken Lu <ken@seeice.com>
2014-02-03 09:18:34 -0500

    finished 350 rest api versioning

--- bay9/hlee/350_rest_api_versioning/config/routes.rb
+++ bay9/hlee/350_rest_api_versioning/config/routes.rb
@@ -1,12 +1,15 @@
+require 'api_constraints'
+
 Store::Application.routes.draw do
   namespace :api, defaults: {format: 'json'} do
-    namespace :v1 do
+    scope module: :v1, constraints: ApiConstraints.new(version: 1) do
       resources :products
     end
-    namespace :v2 do
+    scope module: :v2, constraints: ApiConstraints.new(version: 2, default: :true) do
       resources :products
     end
   end
+  
   resources :products
   root to: 'products#index'
 end
